13a1887817133bcfed8ab66dd96a8e91ba86d352
SimDifRandomHD <hi@simdifrandomhd.xyz>
2023-04-03 06:21:26 +1000

    Update completed.php

--- completed.php
+++ completed.php
@@ -66,12 +66,7 @@
             }
 
             // Connect to the database
-            $servername = "(REDACTED)";
-            $username = "(REDACTED)";
-            $password = "(REDACTED)";
-            $dbname = "(REDACTED)";
-
-            $conn = new mysqli($servername, $username, $password, $dbname);
+			include('db_connect.php');
 
             // Check if the connection is successful
             if ($conn->connect_error) {
