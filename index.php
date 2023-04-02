aabc574c4ad8f5a2861f2df4b2a9c27d5c95f981
SimDifRandomHD <hi@simdifrandomhd.xyz>
2023-04-03 05:32:03 +1000

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 	<main>
 		<p>With this website you can keep track of all the tasks you need to complete.</p>
 		<p>To get started, simply log in with your Twitch account.</p>
-		<a href="https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&scope=user:read:email"><button class="twitch-button">Log in with Twitch</button></a>
-		<a href="todo.php"><button class="twitch-button">OLD SYSTEM LOGIN</button></a>
+		<a href="#"><button class="twitch-button">Log in with Twitch (COMING SOON)</button></a>
+		<a href="todo.php"><button class="button">OLD SYSTEM LOGIN</button></a>
 	</main>
 
 	<footer>
