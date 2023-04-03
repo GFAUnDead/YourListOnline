86d52e63597eed3859f8e8d0c60ce4a73b092341
SimDifRandomHD <lachlan@lochstudios.com>
2023-04-03 18:11:56 +1000

    Update register.php

--- register.php
+++ register.php
@@ -66,7 +66,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Redirect to login page
-                header("location: login.php");
+                header("location: dashboard.php");
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
