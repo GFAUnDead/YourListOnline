85dbf8527138eca063c1dd93422292adf81f5312
SimDifRandomHD <lachlan@lochstudios.com>
2023-04-03 17:29:24 +1000

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,107 +1,46 @@
+<?php
+session_start();
+
+// check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+  header('Location: login.php');
+  exit();
+}
+
+// connect to database
+require_once 'db_connect.php';
+
+// get user ID from session
+$user_id = $_SESSION['user_id'];
+
+// get form data
+$title = $_POST['title'];
+$description = $_POST['description'];
+
+// prepare and execute query
+$stmt = $conn->prepare("INSERT INTO todos (user_id, title, description) VALUES (?, ?, ?)");
+$stmt->bind_param("iss", $user_id, $title, $description);
+$stmt->execute();
+
+// redirect back to dashboard
+header('Location: dashboard.php');
+exit();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>To Do List - Insert</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="icon" href="../../images/logo.png" type="image/png" />
+  <title>YourListOnline - Add New Items</title>
 </head>
 <body>
-	<header>
-		<div class="header-title">
-			<h1>To Do List</h1>
-		</div>
-		<div class="menu">
-            <button onclick="location.href='todo.php'">Home</button>
-		</div>
-	</header>
-
-    <div class="container">
-        <br>
-            <?php
-            // Check if the API key is provided in the URL
-            if (!isset($_GET['api'])) {
-                echo "API key is missing.";
-                exit();
-            }
-            
-            // Retrieve the API key from the URL
-            $api_key = $_GET['api'];
-            
-            // Connect to the API keys database
-            $servername = "(REDACTED)";
-            $username = "(REDACTED)";
-            $password = "(REDACTED)";
-            $dbname = "(REDACTED)";
-            
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            
-            // Check if the connection is successful
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            // Prepare the SQL statement to retrieve the channel name associated with the provided API key
-            $stmt = $conn->prepare("SELECT channelname FROM allowed_users WHERE api_key = ?");
-            $stmt->bind_param("s", $api_key);
-
-            // Execute the SQL statement
-            $stmt->execute();
-            
-            // Bind the result to variables
-            $stmt->bind_result($channelname);
-            
-            // Fetch the result
-            $stmt->fetch();
-            
-            // Close the statement
-            $stmt->close();
-            
-            // Close the database connection
-            $conn->close();
-            
-            // Check if the provided API key is valid and retrieve the channel name from the database
-            if (empty($channelname)) {
-                // Return an error message if the API key is not valid
-                echo "Invalid API key.";
-                exit();
-            }
-            
-            // Check if the form to add a task was submitted
-            if (isset($_POST['todo_text'])) {
-                // Connect to the database
-			    include('db_connect.php');
-
-                // Check if the connection is successful
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-
-                // Prepare the SQL statement to insert the task into the database
-                $stmt = $conn->prepare("INSERT INTO todos (user_id, todo_text, completed) VALUES ((SELECT id FROM users WHERE name=?), ?, "false")");
-                $stmt->bind_param("ss", $channelname, $_POST['todo_text']);
-            
-                // Execute the SQL statement
-                if ($stmt->execute()) {
-                    // Display a success message to the user
-                    echo "Task added successfully!";
-                } else {
-                    // If the SQL statement fails, display an error message to the user
-                    echo "An error occurred while adding the task. Please try again later.";
-                }
-
-                // Close the statement
-                $stmt->close();
-            
-                // Close the database connection
-                $conn->close();
-            }
-            
-            echo "<form method='POST' action=''>";
-            echo "<label for='todo_text'>Task:</label><br>";
-            echo "<input type='text' id='todo_text' name='todo_text'><br>";
-            echo "<input type='submit' value='Add task'>";
-            echo "</form>";
-            ?>
-	</div>
+  <h1>Add New Todo Item</h1>
+  <form method="post">
+    <label for="title">Title:</label>
+    <input type="text" id="title" name="title" required><br><br>
+    <label for="description">Description:</label>
+    <textarea id="description" name="description"></textarea><br><br>
+    <input type="hidden" name="user_id" value="<?php echo $_SESSION["user_id"]; ?>">
+    <input type="submit" value="Add">
+  </form>
 </body>
 </html>
