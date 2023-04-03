27f121cb5dcd89929b096ab85af3b7b8a5d44a1a
GFAUnDead <hi@simdifrandomhd.xyz>
2023-04-04 09:06:52 +1000

    Update insert.php

--- insert.php
+++ insert.php
@@ -33,6 +33,21 @@ exit();
   <title>YourListOnline - Add New Items</title>
 </head>
 <body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">YourListOnline</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li class="active"><a href="insert.php">Add</a></li>
+                <li><a href="completed.php">Completed</a></li>
+                <li><a href="update.php">Update</a></li>
+                <li><a href="remove.php">Remove</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </nav>
   <h1>Add New Todo Item</h1>
   <form method="post">
     <label for="title">Title:</label>
