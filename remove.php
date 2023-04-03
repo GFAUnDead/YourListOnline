8556bce9d330bae59dca97a06f658e54fcc18282
SimDifRandomHD <lachlan@lochstudios.com>
2023-04-03 18:11:58 +1000

    Update remove.php

--- remove.php
+++ remove.php
@@ -1,174 +1,69 @@
+<?php
+// Start session
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+// Connect to database
+require_once "db_connect.php";
+
+// Get user's to-do list
+$user_id = $_SESSION['user_id'];
+$sql = "SELECT * FROM todos WHERE user_id = $user_id ORDER BY created_at DESC";
+$result = $conn->query($sql);
+
+// Handle remove item form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $todo_id = $_POST['todo_id'];
+
+    // Delete item from database
+    $sql = "DELETE FROM todos WHERE id = $todo_id";
+    $result = $conn->query($sql);
+
+    // Redirect back to remove page
+    header('Location: remove.php');
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>YourListOnline - Remove</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="icon" href="../../images/logo.png" type="image/png" />
+    <title>YourListOnline - Remove Item</title>
 </head>
 <body>
-	<header>
-		<div class="header-title">
-			<h1>To Do List</h1>
-		</div>
-		<div class="menu">
-            <button onclick="location.href='todo.php'">Home</button>   
-		</div>
-	</header>
+    <h1>Remove Item</h1>
 
-    <div class="container">
-        <br>
-        <?php
-            // Check if the API key is provided in the URL
-            if (!isset($_GET['api'])) {
-                echo "API key is missing.";
-                exit();
-            }
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Created At</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?= $row['title'] ?></td>
+                    <td><?= $row['description'] ?></td>
+                    <td><?= $row['created_at'] ?></td>
+                    <td>
+                        <form method="POST">
+                            <input type="hidden" name="todo_id" value="<?= $row['id'] ?>">
+                            <button type="submit">Remove</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
 
-            // Retrieve the API key from the URL
-            $api_key = $_GET['api'];
-
-            // Connect to the API keys database
-            $servername = "(REDACTED)";
-            $username = "(REDACTED)";
-            $password = "(REDACTED)";
-            $dbname = "(REDACTED)";
-
-            $conn = new mysqli($servername, $username, $password, $dbname);
-
-            // Check if the connection is successful
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            // Prepare the SQL statement to retrieve the channel name from the database
-            $stmt = $conn->prepare("SELECT channelname FROM allowed_users WHERE api_key = ?");
-            $stmt->bind_param("s", $api_key);
-
-            // Execute the SQL statement
-            $stmt->execute();
-
-            // Bind the result to variables
-            $stmt->bind_result($channelname);
-
-            // Fetch the result
-            $stmt->fetch();
-
-            // Close the statement
-            $stmt->close();
-
-            // Close the database connection
-            $conn->close();
-
-            // Check if the provided API key is valid and retrieve the channel name from the database
-            if (empty($channelname)) {
-                // Return an error message if the API key is not valid
-                echo "Invalid API key.";
-                exit();
-            }
-
-            // Connect to the database
-			include('db_connect.php');
-
-            // Check if the connection is successful
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            // Check if the form is submitted
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                // Retrieve the task ID and user ID from the form data
-                $task_id = $_POST['todo_id'];
-                $user_id = isset($_POST['user_id']) ? $_POST['user_id'] : '';
-            
-                if (!empty($user_id)) {
-                    // Prepare the SQL statement to delete the task from the database
-                    $stmt = $conn->prepare("DELETE FROM todos WHERE id = ? AND user_id = ?");
-                    $stmt->bind_param("ii", $task_id, $user_id);
-                
-                    // Execute the SQL statement
-                    if ($stmt->execute()) {
-                        if ($stmt->affected_rows > 0) {
-                            echo "Task deleted successfully.";
-                        } else {
-                            echo "You are not authorized to delete this task.";
-                        }
-                    } else {
-                        echo "Error deleting task: " . $stmt->error;
-                    }
-                
-                    // Close the statement
-                    $stmt->close();
-                } else {
-                    echo "User ID is missing.";
-                }
-            }
-
-            // Prepare the SQL statement to retrieve the user's tasks from the database
-            $stmt = $conn->prepare("SELECT id, todo_text FROM todos WHERE user_id=(SELECT id FROM users WHERE name=?)");
-            $stmt->bind_param("s", $channelname);
-
-            // Execute the SQL statement
-            $stmt->execute();
-
-            // Bind the result to variables
-            $stmt->bind_result($task_id, $task_text);
-
-            // Output the tasks in an HTML table
-            echo "<table>";
-            echo "<tr><th>Task ID</th><th>Task Text</th></tr>";
-            while ($stmt->fetch()) {
-                echo "<tr><td>$task_id</td><td>$task_text</td></tr>";
-            }
-            echo "</table>";
-
-            // Close the statement
-            $stmt->close();
-            
-            // Connect to the database
-			include('db_connect.php');
-
-            // Check if the connection is successful
-            if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-            }
-            // Prepare the SQL statement to retrieve the user's ID from the database
-            $stmt = $conn->prepare("SELECT id FROM users WHERE name = ?");
-            $stmt->bind_param("s", $channelname);
-
-            // Execute the SQL statement
-            $stmt->execute();
-
-            // Bind the result to variables
-            $stmt->bind_result($current_user_id);
-
-            // Fetch the result
-            $stmt->fetch();
-
-            // Close the statement
-            $stmt->close();
-
-            $stmt = $conn->prepare("SELECT users.id FROM users INNER JOIN todos ON users.id = todos.user_id WHERE todos.id = ?");
-            $stmt->bind_param("i", $task_id);
-
-            // Execute the SQL statement
-            $stmt->execute();
-
-            // Bind the result to variables
-            $stmt->bind_result($user_id);
-
-            // Fetch the result
-            $stmt->fetch();
-
-            // Close the statement
-            $stmt->close();
-
-            echo "<form method='POST'>";
-            echo "<label for='todo_id'>Task ID:</label>";
-            echo "<input type='number' name='todo_id' required><br><br>";
-            echo "<input type='hidden' name='user_id' value='$user_id'>";
-            echo "<input type='submit' name='delete' value='Delete Task'>";
-            echo "</form>";
-        ?>
-	</div>
+    <p><a href="dashboard.php">Back to Dashboard</a></p>
 </body>
 </html>
