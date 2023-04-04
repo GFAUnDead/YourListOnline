53f85dcdbbe3658f7221d740675d853d229d1974
GFAUnDead <hi@simdifrandomhd.xyz>
2023-04-05 07:54:45 +1000

    Update remove.php

--- remove.php
+++ remove.php
@@ -34,6 +34,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>YourListOnline - Remove Item</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
 </head>
 <body>
     <nav class="navbar navbar-default">
