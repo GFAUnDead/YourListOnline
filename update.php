f855e8d68f28eb25231adab6e7b3c9cf90218b03
GFAUnDead <hi@simdifrandomhd.xyz>
2023-04-05 08:01:44 +1000

    Update update.php

--- update.php
+++ update.php
@@ -1,23 +1,40 @@
+<?php
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['loggedin'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>YourListOnline - Update</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="icon" href="../../images/logo.png" type="image/png" />
+    <title>YourListOnline - Update</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
 </head>
 <body>
-	<header>
-		<div class="header-title">
-			<h1>To Do List</h1>
-		</div>
-		<div class="menu">
-            <button onclick="location.href='todo.php'">Home</button>
-		</div>
-	</header>
-
-    <div class="container">
-        <br>
-
-	</div>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">YourListOnline</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="insert.php">Add</a></li>
+                <li><a href="completed.php">Completed</a></li>
+                <li class="active"><a href="update.php">Update</a></li>
+                <li><a href="remove.php">Remove</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    <h1>My To-Do List</h1>
 </body>
 </html>
