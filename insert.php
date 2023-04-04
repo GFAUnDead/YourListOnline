f11dc61f05c2c119616a90fb58f7b4069a80c958
GFAUnDead <hi@simdifrandomhd.xyz>
2023-04-05 08:07:46 +1000

    Update insert.php

--- insert.php
+++ insert.php
@@ -47,6 +47,7 @@ $stmt->execute();
                 <li><a href="remove.php">Remove</a></li>
                 <li><a href="logout.php">Logout</a></li>
             </ul>
+            <p class="navbar-text navbar-right">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</p>
         </div>
     </nav>
   <h1>Add New Todo Item</h1>
