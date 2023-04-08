9b17696c641824de196753749e7231c2a55f90bb
gfaUnDead <contact@gfaundead.stream>
2023-04-08 19:48:21 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -40,8 +40,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <title>YourListOnline - Profile</title>
   <link rel="icon" href="img/logo.png" type="image/png" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <link rel="stylesheet" href="css/style.css">
   <script src="js/about.js"></script>
+  <script src="js/profile.js"></script>
   <style type="text/css">
     body {
       font: 14px sans-serif;
@@ -74,38 +76,16 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </div>
   </nav>
   <h1>Your Profile</h1>
-  <div class="wrapper">
-    <p><strong>Username:</strong> <?php echo $_SESSION['username']; ?></p>
-    <p><strong>Joined:</strong> <?php echo date('F j, Y', strtotime($_SESSION['signup_date'])); ?></p>
-    <p><strong>Last Login:</strong> <?php echo date('F j, Y', strtotime($_SESSION['last_login'])); ?> at <?php echo date('g:i A', strtotime($last_login)); ?></p>
-    <p><strong>API Key:</strong>
-      <div class="api-key-wrapper" style="display: none;">
-      <?php echo $api_key; ?>
-      </div>
-    </p>
-    <button type="button" class="btn btn-primary" id="show-api-key">Show API Key</button>
-    <button type="button" class="btn btn-primary" id="hide-api-key" style="display:none;">Hide API Key</button>
-    <br><br>
-    <a href="change_password.php" class="btn btn-primary">Change Password</a>
-    <a href="logout.php" class="btn btn-primary">Logout</a>
+<div class="wrapper">
+  <p><strong>Username:</strong> <?php echo $_SESSION['username']; ?></p>
+  <p><strong>Joined:</strong> <?php echo date('F j, Y', strtotime($_SESSION['signup_date'])); ?></p>
+  <p><strong>Last Login:</strong> <?php echo date('F j, Y', strtotime($_SESSION['last_login'])); ?> at <?php echo date('g:i A', strtotime($last_login)); ?></p>
+  <p><strong>API Key:</strong> <span class="api-key-wrapper" style="display: none;"><?php echo $api_key; ?></span></p>
+  <button type="button" class="btn btn-primary" id="show-api-key">Show API Key</button>
+  <button type="button" class="btn btn-primary" id="hide-api-key" style="display:none;">Hide API Key</button>
+  <br><br>
+  <a href="change_password.php" class="btn btn-primary">Change Password</a>
+  <a href="logout.php" class="btn btn-primary">Logout</a>
 </div>
-
-<script>
-    var showApiKeyButton = document.getElementById('show-api-key');
-    var hideApiKeyButton = document.getElementById('hide-api-key');
-    var apiKeyWrapper = document.querySelector('.api-key-wrapper');
-
-    showApiKeyButton.addEventListener('click', function() {
-        apiKeyWrapper.style.display = 'block';
-        showApiKeyButton.style.display = 'none';
-        hideApiKeyButton.style.display = 'block';
-    });
-
-    hideApiKeyButton.addEventListener('click', function() {
-        apiKeyWrapper.style.display = 'none';
-        showApiKeyButton.style.display = 'block';
-        hideApiKeyButton.style.display = 'none';
-    });
-</script>
 </body>
 </html>
