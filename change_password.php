4de62d40d2c410463c12c27124e5d91260a26d52
gfaUnDead <contact@gfaundead.stream>
2023-04-08 18:33:36 +1000

    Update change_password.php

--- change_password.php
+++ change_password.php
@@ -96,23 +96,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">YourListOnline</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="insert.php">Add</a></li>
-                <li><a href="completed.php">Completed</a></li>
-                <li><a href="update.php">Update</a></li>
-                <li><a href="remove.php">Remove</a></li>
-                <li class="active"><a href="change_password.php">Password Change</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-            <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">YourListOnline</a>
         </div>
-    </nav>
+        <ul class="nav navbar-nav">
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="insert.php">Add</a></li>
+            <li><a href="completed.php">Completed</a></li>
+            <li><a href="update.php">Update</a></li>
+            <li><a href="remove.php">Remove</a></li>
+            <li><a href="profile.php">Profile</a></li>
+        </ul>
+        <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
+    </div>
+</nav>
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
