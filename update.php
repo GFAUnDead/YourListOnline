da5f6c8de34ca3ad7dbcdd04e919c847e5f75fce
SimDifRandomHD <lachlan@lochstudios.com>
2023-04-08 13:23:12 +1000

    Update update.php

--- update.php
+++ update.php
@@ -19,22 +19,23 @@ if (!isset($_SESSION['loggedin'])) {
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">YourListOnline</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="insert.php">Add</a></li>
-                <li><a href="completed.php">Completed</a></li>
-                <li class="active"><a href="update.php">Update</a></li>
-                <li><a href="remove.php">Remove</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-            <p class="navbar-text navbar-right">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</p>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">YourListOnline</a>
         </div>
-    </nav>
+        <ul class="nav navbar-nav">
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="insert.php">Add</a></li>
+            <li><a href="completed.php">Completed</a></li>
+            <li class="active"><a href="update.php">Update</a></li>
+            <li><a href="remove.php">Remove</a></li>
+            <li><a href="change_password.php">Password Change</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+        <p class="navbar-text navbar-right">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</p>
+    </div>
+  </nav>
     
     <h1>My To-Do List</h1>
 </body>
