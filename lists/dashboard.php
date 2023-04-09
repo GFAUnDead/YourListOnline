cf4974f310cd5be4aad314140fc5fb64386fc7d8
gfaUnDead <contact@gfaundead.stream>
2023-04-09 16:06:46 +1000

    Update dashboard.php
    Update home link

--- lists/dashboard.php
+++ lists/dashboard.php
@@ -52,7 +52,7 @@ if (!$result) {
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="index.php">YourListOnline</a>
+          <a class="navbar-brand" href="../index.php">YourListOnline</a>
         </div>
         <ul class="nav navbar-nav">
             <li class="active"><a href="dashboard.php">Dashboard</a></li>
