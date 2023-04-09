f83ba19a631e5767c8c03a2c4b1749794390d584
gfaUnDead <contact@gfaundead.stream>
2023-04-09 16:06:44 +1000

    Update completed.php
    Update home link

--- lists/completed.php
+++ lists/completed.php
@@ -60,7 +60,7 @@ if (isset($_POST['task_id'])) {
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="index.php">YourListOnline</a>
+        <a class="navbar-brand" href="../index.php">YourListOnline</a>
         </div>
         <ul class="nav navbar-nav">
             <li><a href="dashboard.php">Dashboard</a></li>
