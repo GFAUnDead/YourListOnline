a04689d921453d557c5ef512bf7a5c946f851f88
gfaUnDead <contact@gfaundead.stream>
2023-04-11 00:34:45 +1000

    Update add_category.php

--- lists/add_category.php
+++ lists/add_category.php
@@ -81,3 +81,59 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mysqli->close();
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>YourListOnline - Add Category</title>
+    <link rel="icon" href="img/logo.png" type="image/png" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/about.js"></script>
+    <style type="text/css">
+      body {
+        font: 14px sans-serif;
+      }
+      .wrapper {
+        width: 350px; padding: 20px;
+      }
+      a.popup-link {
+        text-decoration: none;
+        color: black;
+        cursor: pointer;
+      }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+        <a class="navbar-brand" href="../index.php">YourListOnline</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="insert.php">Add</a></li>
+            <li><a href="completed.php">Completed</a></li>
+            <li><a href="update.php">Update</a></li>
+            <li><a href="remove.php">Remove</a></li>
+            <li><a href="profile.php">Profile</a></li>
+        </ul>
+        <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
+    </div>
+</nav>
+<div class="wrapper">
+    <h2>Add Category</h2>
+    <p>Please fill in the form below to add a new category.</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group <?php echo (!empty($category_err)) ? 'has-error' : ''; ?>">
+            <label>Category</label>
+            <input type="text" name="category" class="form-control" value="<?php echo htmlspecialchars($category); ?>">
+            <span class="help-block"><?php echo $category_err; ?></span>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Submit">
+            <a href="categories.php" class="btn btn-default">Cancel</a>
+        </div>
+    </form>
+</div>
+</body>
+</html>
