987296bd10fbe17560c779197bbc1ab90740acc6
gfaUnDead <contact@gfaundead.stream>
2023-04-11 16:52:42 +1000

    Update insert.php

--- lists/insert.php
+++ lists/insert.php
@@ -62,13 +62,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <li><a href="completed.php">Completed</a></li>
             <li><a href="update.php">Update</a></li>
             <li><a href="remove.php">Remove</a></li>
-            <li><a href="categories.php">View Categories</a></li>
-            <li><a href="add_category.php">Add Category</a></li>
+            <li class="dropdown dropdown-hover" data-toggle="dropdown">
+                <a>Categories <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="categories.php">View Categories</a></li>
+                    <li><a href="add_category.php">Add Category</a></li>
+                </ul>
+            </li>
             <li><a href="profile.php">Profile</a></li>
         </ul>
         <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
     </div>
-  </nav>
+</nav>
   <div class="col-md-6">
   <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
   <h1>Please enter your task to add it to your list:</h1>
