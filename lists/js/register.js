3c4e8e7c6aabb9403ca833ce857d4405e8fa701c
gfaUnDead <contact@gfaundead.stream>
2023-04-11 18:03:50 +1000

    Update register.js

--- lists/js/register.js
+++ lists/js/register.js
@@ -1,25 +1,7 @@
-// Get the registration form element
-const registrationForm = document.getElementById('registration-form');
+const registrationEnabled = false; // Replace this with the actual PHP code that checks whether registration is enabled or not
 
-// Add an event listener for when the form is submitted
-registrationForm.addEventListener('submit', function(event) {
-  // Prevent the default form submission behavior
-  event.preventDefault();
-  
-  // Get the value of the username field
-  const username = document.getElementById('username').value;
-  
-  // Get the value of the password field
-  const password = document.getElementById('password').value;
-  
-  // Check if the registration is enabled
-  if (registration_enabled) {
-    // If registration is enabled, submit the form
-    registrationForm.submit();
-  } else {
-    // If registration is disabled, display an error message
-    const errorMessage = document.createElement('p');
-    errorMessage.innerText = 'Registration is currently disabled.';
-    registrationForm.appendChild(errorMessage);
-  }
-});
+if (!registrationEnabled) {
+  const errorMessage = document.getElementById('error-message');
+  errorMessage.textContent = 'Registration is currently disabled.';
+  errorMessage.style.color = 'red';
+}
