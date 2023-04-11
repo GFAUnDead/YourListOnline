cd536b1715e4cb4a373913f53469fd5d72c1f5b5
gfaUnDead <contact@gfaundead.stream>
2023-04-11 17:06:02 +1000

    Update dashboard.php

--- lists/dashboard.php
+++ lists/dashboard.php
@@ -62,8 +62,8 @@ if (!$result) {
             <li><a href="completed.php">Completed</a></li>
             <li><a href="update.php">Update</a></li>
             <li><a href="remove.php">Remove</a></li>
-            <li class="dropdown dropdown-hover" data-toggle="dropdown">
-                <a>Categories <span class="caret"></span></a>
+            <li class="dropdown dropdown-hover">
+                <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li><a href="categories.php">View Categories</a></li>
                     <li><a href="add_category.php">Add Category</a></li>
