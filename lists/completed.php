eac4dccdb2e480e368718b9d56b18d4cd4134cbe
gfaUnDead <contact@gfaundead.stream>
2023-04-11 16:50:33 +1000

    Update completed.php

--- lists/completed.php
+++ lists/completed.php
@@ -40,6 +40,8 @@ if (isset($_POST['task_id'])) {
     <title>YourListOnline - Completed</title>
     <link rel="icon" href="img/logo.png" type="image/png" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/about.js"></script>
     <style type="text/css">
