92dd7a35c3ef54bfcd7bcbf43031f82079931b4c
gfaUnDead <contact@gfaundead.stream>
2023-04-11 16:50:21 +1000

    Update register.php

--- lists/register.php
+++ lists/register.php
@@ -94,6 +94,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>YourListOnline - Sign Up</title>
     <link rel="icon" href="img/logo.png" type="image/png" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/about.js"></script>
     <style type="text/css">
