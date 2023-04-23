44b713ca92d7c11d55a21c85e0f5de8c2c454bc1
gfaUnDead <contact@gfaundead.stream>
2023-04-23 13:13:22 +1000

    Update index.php

--- index.php
+++ index.php
@@ -17,16 +17,13 @@
 	<header>
 		<h1>Welcome to Your List Online</h1>
 	</header>
-	
 	<main>
 		<p>With this website you can keep track of all the tasks you need to complete.</p>
 		<p>To get started, simply log in with your Twitch account.</p>
-		<a href="#"><button class="twitch-button">Log in with Twitch (COMING SOON)</button></a>
-		<a href="lists/dashboard.php"><button class="button">OLD SYSTEM LOGIN</button></a>
+		<a href="lists/dashboard.php"><button class="button">Login</button></a>
+		<a href=""><button class="twitch-button">Login with Twitch (COMING SOON)</button></a>
+		<br><br>
+		<p><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
 	</main>
-
-	<footer>
-	<p><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
-	</footer>
 </body>
 </html>
