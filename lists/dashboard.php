bf9cf7a631d8d5c2e3dd7e8a2d35de8737024e0b
gfaUnDead <contact@gfaundead.stream>
2023-04-24 23:32:35 +1000

    Update dashboard.php

--- lists/dashboard.php
+++ lists/dashboard.php
@@ -48,6 +48,7 @@ if (!$result) {
 <head>
   <title>YourListOnline - Dashboard</title>
   <link rel="icon" href="img/logo.png" type="image/png" />
+  <link rel="apple-touch-icon" href="img/logo.png">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
