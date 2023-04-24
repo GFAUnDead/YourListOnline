9a331dac19c9e07fe0ad38cc83aa3dee74e4fb9c
gfaUnDead <contact@gfaundead.stream>
2023-04-24 21:54:49 +1000

    Update dashboard.php

--- lists/dashboard.php
+++ lists/dashboard.php
@@ -91,6 +91,7 @@ if (!$result) {
         <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
     </div>
 </nav>
+<div class="col-md-6">
     <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
     <h2>Your Current List:</h2>
     <?php echo "Number of rows in your list: " . mysqli_num_rows($result); ?>    
@@ -116,5 +117,6 @@ if (!$result) {
         <?php endwhile; ?>
       </tbody>
     </table>
+</div>
 </body>
 </html>
