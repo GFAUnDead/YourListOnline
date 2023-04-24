e6535b898fa1907083f263355bd10f8a9d462185
gfaUnDead <contact@gfaundead.stream>
2023-04-24 21:54:38 +1000

    Update categories.php

--- lists/categories.php
+++ lists/categories.php
@@ -66,6 +66,7 @@ if (!$result) {
         <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
     </div>
 </nav>
+<div class="col-md-6">
   <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
   <h1>Here is the current list of categories you can filter your lists in, each category will be it's own list.</h1>
   <table class="table">
@@ -82,5 +83,6 @@ if (!$result) {
           <?php endwhile ?>
       </tbody>
   </table>
+</div>
 </body>
 </html>
