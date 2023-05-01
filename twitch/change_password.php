ed76af96ebf3ef2952ca19cc1f5b788c391c2631
GFAUnDead <contact@gfaundead.stream>
2023-05-02 08:35:23 +1000

    Create change_password.php

---
+++ twitch/change_password.php
@@ -0,0 +1,161 @@
+<?php
+session_start();
+if (!isset($_SESSION['twitchloggedin'])) {
+    header("Location: logincall.php");
+    exit();
+}
+
+// Require database connection
+require_once "db_connect.php";
+
+// Define variables and initialize with empty values
+$current_password = $new_password = $confirm_password = "";
+$current_password_err = $new_password_err = $confirm_password_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    // Validate current password
+    if (empty(trim($_POST["current_password"]))) {
+        $current_password_err = "Please enter your current password.";
+    } else {
+        $current_password = trim($_POST["current_password"]);
+    }
+
+    // Validate new password
+    if (empty(trim($_POST["new_password"]))) {
+        $new_password_err = "Please enter a new password.";
+    } elseif (strlen(trim($_POST["new_password"])) < 8) {
+        $new_password_err = "Password must have at least 8 characters.";
+    } else {
+        $new_password = trim($_POST["new_password"]);
+    }
+
+    // Validate confirm password
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm the new password.";
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if (empty($new_password_err) && ($new_password !== $confirm_password)) {
+            $confirm_password_err = "Passwords do not match.";
+        }
+    }
+
+    // Check input errors before updating the database
+    if (empty($current_password_err) && empty($new_password_err) && empty($confirm_password_err)) {
+        // Check current password
+        $username = $_SESSION['username'];
+        $sql = "SELECT password FROM users WHERE username = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if ($result) {
+            if (mysqli_num_rows($result) == 1) {
+                $row = mysqli_fetch_assoc($result);
+                $hashed_password = $row["password"];
+                if (password_verify($current_password, $hashed_password)) {
+                    // Update password
+                    $hashed_new_password = password_hash($new_password, PASSWORD_DEFAULT);
+                    $sql = "UPDATE users SET password = ? WHERE username = ?";
+                    $stmt = mysqli_prepare($conn, $sql);
+                    mysqli_stmt_bind_param($stmt, "ss", $hashed_new_password, $username);
+                    mysqli_stmt_execute($stmt);
+                    if ($stmt->affected_rows > 0) {
+                        // Password updated successfully, redirect to login page
+                        header("Location: logincall.php");
+                        exit();
+                    } else {
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+                } else {
+                    $current_password_err = "The password you entered is not valid.";
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        } else {
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+
+    // Close database connection
+    mysqli_close($conn);
+    header("Location: logout.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Change Password - YourListOnline</title>
+    <link rel="icon" href="img/logo.png" type="image/png" />
+    <link rel="apple-touch-icon" href="img/logo.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/about.js"></script>
+    <style type="text/css">
+      a.popup-link {
+        text-decoration: none;
+        color: black;
+        cursor: pointer;
+      }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="../index.php">YourListOnline</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="insert.php">Add</a></li>
+            <li><a href="completed.php">Completed</a></li>
+            <li><a href="update.php">Update</a></li>
+            <li><a href="remove.php">Remove</a></li>
+            <li class="dropdown dropdown-hover">
+                <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="categories.php">View Categories</a></li>
+                    <li><a href="add_category.php">Add Category</a></li>
+                </ul>
+            </li>
+            <li class="dropdown dropdown-hover">
+			      <a class="dropdown" data-toggle="dropdown">Profile <span class="caret"></span></a>
+			      	<ul class="dropdown-menu">
+			      		<li><a href="profile.php">View Profile</a></li>
+			      		<li><a href="update_profile.php">Update Profile</a></li>
+			      	</ul>
+            </li>
+        </ul>
+        <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
+    </div>
+</nav>
+<div class="col-md-6">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <h2>Change Password</h2>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <div class="form-group">
+                    <label for="current_password">Current Password:</label>
+                    <input type="password" class="form-control" id="current_password" name="current_password" required>
+                </div>
+                <div class="form-group">
+                    <label for="new_password">New Password:</label>
+                    <input type="password" class="form-control" id="new_password" name="new_password" required>
+                </div>
+                <div class="form-group">
+                    <label for="confirm_password">Confirm Password:</label>
+                    <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
