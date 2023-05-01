fe602a6ee3958da0b821e715db4c83afc45cc426
GFAUnDead <contact@gfaundead.stream>
2023-05-01 15:24:45 +1000

    Create roadmap.php

---
+++ roadmap.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>YourListOnline - Road Map</title>
+	<link rel="icon" href="img/logo.png" type="image/png" />
+	<link rel="apple-touch-icon" href="img/logo.png">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="js/about.js"></script>
+	<style type="text/css">
+    a.popup-link {
+      text-decoration: none;
+      color: black;
+      cursor: pointer;
+    }
+  </style>
+</head>
+<body>
+	<header>
+		<h1>Welcome to Your List Online Road Map</h1>
+	</header>
+	<main>
+	<ul>
+		<li>Formatting and Styling:
+			<ul>
+				<li>Allow the use of colours for the OBS link to make it stand out.</li>
+				<li>Use bold, underline or italics to emphasize important information in the text.</li>
+				<li>Use dot points or numbered points to organize information and make it easy to follow.</li>
+			</ul>
+		</li>
+		<li>Colab List:
+			<ul>
+				<li>Create a list of collaborators and their roles.</li>
+				<li>Define the tasks and responsibilities of each collaborator.</li>
+				<li>Set deadlines for each task.</li>
+			</ul>
+		</li>
+		<li>Register/Login Page:
+			<ul>
+				<li>Implement automatic profile image generation for users who register or login.</li>
+				<li>Allow users to choose a profile picture from their device or social media profiles.</li>
+			</ul>
+		</li>
+		<li>Mobile Support:
+			<ul>
+				<li>Optimize the website for mobile devices, making it easy to navigate and use.</li>
+				<li>Use responsive design to ensure that the website looks good on a variety of screen sizes.</li>
+				<li>Test the website on multiple mobile devices to ensure it works as expected.</li>
+			</ul>
+		</li>
+		<li>Twitch Login Support:
+			<ul>
+				<li>Allow users to login with their Twitch account.</li>
+				<li>Use Twitch's API to retrieve user information and display it on the website.</li>
+			</ul>
+		</li>
+	</ul>
+	</main>
+</body>
+</html>
