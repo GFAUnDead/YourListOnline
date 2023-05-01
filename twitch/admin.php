5283ff203ad1cbf275fa9ce08d0363b6bbf028ef
GFAUnDead <contact@gfaundead.stream>
2023-05-02 08:36:52 +1000

    Update admin.php

--- twitch/admin.php
+++ twitch/admin.php
@@ -1,5 +1,5 @@
-<?php ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); ?>
 <?php
+// Initialize the session
 session_start();
 
 // Check if user is logged in
