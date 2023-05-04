65264b123e1375fc0563d17c4e7278cb17c84d60
GFAUnDead <contact@gfaundead.stream>
2023-05-05 00:19:14 +1000

    Update profile.php

--- twitch/profile.php
+++ twitch/profile.php
@@ -3,9 +3,9 @@
 session_start();
 
 // Check if the user is already logged in
-if(!isset($_SESSION["twitchloggedin"]) || $_SESSION["twitchloggedin"] !== true){
-    header("location: logincall.php");
-    exit();
+if(!isset($_SESSION["access_token"]) || $_SESSION["access_token"] !== true){
+    header("location: login.php");
+    exit;
 } else {
     // Require database connection
     require_once "db_connect.php";
