82cc6cd932e80f69a25a130828066a09cb7d5064
GFAUnDead <contact@gfaundead.stream>
2023-05-05 00:11:18 +1000

    Update dashboard.php

--- twitch/dashboard.php
+++ twitch/dashboard.php
@@ -68,7 +68,7 @@ if (!$result) {
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="../index.php">YourListOnline</a>
+          <a class="navbar-brand" href="https://yourlist.online/">YourListOnline</a>
         </div>
         <ul class="nav navbar-nav">
             <li class="active"><a href="dashboard.php">Dashboard</a></li>
