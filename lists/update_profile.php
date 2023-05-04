e16ee72ad4bbfe8567648427a1278d4af0c6c50e
GFAUnDead <contact@gfaundead.stream>
2023-05-05 09:49:53 +1000

    Update update_profile.php

--- lists/update_profile.php
+++ lists/update_profile.php
@@ -116,6 +116,9 @@ $conn->close();
                         <li><a href="logout.php">Logout</a></li>
 			      	</ul>
             </li>
+            <?php if ($_SESSION['is_admin']) { ?>
+              <li><a href="admin.php">Admin Page</a></li>
+            <?php } ?>
         </ul>
         <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
     </div>
