7ea71a8ef8828a8db70d6617d486838008634ad5
GFAUnDead <contact@gfaundead.stream>
2023-05-05 00:07:18 +1000

    Update remove.php

--- discord/remove.php
+++ discord/remove.php
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="../index.php">YourListOnline</a>
+            <a class="navbar-brand" href="https://yourlist.online/">YourListOnline</a>
         </div>
         <ul class="nav navbar-nav">
             <li><a href="dashboard.php">Dashboard</a></li>
