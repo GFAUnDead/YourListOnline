1421f53e3cc06b0ff5c54dff610a622d406fe58a
GFAUnDead <contact@gfaundead.stream>
2023-05-05 00:07:23 +1000

    Update update_profile.php

--- discord/update_profile.php
+++ discord/update_profile.php
@@ -93,7 +93,7 @@ $conn->close();
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="../index.php">YourListOnline</a>
+            <a class="navbar-brand" href="https://yourlist.online/">YourListOnline</a>
         </div>
         <ul class="nav navbar-nav">
             <li><a href="dashboard.php">Dashboard</a></li>
