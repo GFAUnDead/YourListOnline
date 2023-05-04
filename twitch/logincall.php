74f2d722f88da23c69dd5f0cef3c1870eddc8b68
GFAUnDead <contact@gfaundead.stream>
2023-05-05 00:11:20 +1000

    Update logincall.php

--- twitch/logincall.php
+++ twitch/logincall.php
@@ -5,7 +5,7 @@ session_start();
 // Check if the user is already logged in, if yes then redirect him to dashboard page
 if(isset($_SESSION["twitchloggedin"]) && $_SESSION["twitchloggedin"] === true){
     header("location: dashboard.php");
-    exit;
+    exit();
 }
 
 // Require database connection
