0a7d5d86ae937cd66252f4de1657116c639369e8
GFAUnDead <contact@gfaundead.stream>
2023-05-05 00:34:27 +1000

    Update dashboard.php

--- lists/dashboard.php
+++ lists/dashboard.php
@@ -88,6 +88,7 @@ if (!$result) {
 			      	<ul class="dropdown-menu">
 			      		<li><a href="profile.php">View Profile</a></li>
 			      		<li><a href="update_profile.php">Update Profile</a></li>
+                <li><a href="logout.php">Logout</a></li>
 			      	</ul>
             </li>
         </ul>
