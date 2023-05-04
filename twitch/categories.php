944ba84e7ef19b4e08b7574e0aa584b2a7fc898f
GFAUnDead <contact@gfaundead.stream>
2023-05-05 00:09:14 +1000

    Update categories.php

--- twitch/categories.php
+++ twitch/categories.php
@@ -48,7 +48,7 @@ if (!$result) {
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="../index.php">YourListOnline</a>
+            <a class="navbar-brand" href="https://yourlist.online/">YourListOnline</a>
         </div>
         <ul class="nav navbar-nav">
             <li><a href="dashboard.php">Dashboard</a></li>
