dd1bc6e13454f07a5fdba26b50252017dd4ddb08
GFAUnDead <contact@gfaundead.stream>
2023-05-05 00:19:00 +1000

    Update admin.php

--- twitch/admin.php
+++ twitch/admin.php
@@ -1,10 +1,11 @@
+<?php ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); ?>
 <?php
 // Initialize the session
 session_start();
 
 // Check if user is logged in
-if (!isset($_SESSION['twitchloggedin'])) {
-  header("Location: logincall.php");
+if (!isset($_SESSION['access_token'])) {
+  header("Location: login.php");
   exit();
 }
 
