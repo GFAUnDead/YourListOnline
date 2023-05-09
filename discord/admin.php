1f567115bcde788a7aa1eaae0ea4065f5b96a4a6
GFAUnDead <contact@gfaundead.stream>
2023-05-10 08:06:40 +1000

    Update admin.php

--- discord/admin.php
+++ discord/admin.php
@@ -82,7 +82,13 @@ if ($_SESSION['is_admin'] == 1) {
             <li><a href="dashboard.php">Dashboard</a></li>
             <li><a href="insert.php">Add</a></li>
             <li><a href="completed.php">Completed</a></li>
-            <li><a href="update.php">Update</a></li>
+            <li class="dropdown dropdown-hover">
+                <a class="dropdown" data-toggle="dropdown">Update <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="update_objective.php">Update Objective</a></li>
+                    <li><a href="update_category.php">Update Category</a></li>
+                </ul>
+            </li>
             <li><a href="remove.php">Remove</a></li>
             <li class="dropdown dropdown-hover">
                 <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
@@ -96,9 +102,17 @@ if ($_SESSION['is_admin'] == 1) {
 			      	<ul class="dropdown-menu">
 			      		<li><a href="profile.php">View Profile</a></li>
 			      		<li><a href="update_profile.php">Update Profile</a></li>
+                <li><a href="logout.php">Logout</a></li>
 			      	</ul>
             </li>
-            <li class="active"><a href="admin.php">Admin Page</a></li>
+            <?php if ($_SESSION['is_admin']) { ?>
+            <li class="dropdown dropdown-hover">
+			      <a class="dropdown" data-toggle="dropdown">Admins <span class="caret"></span></a>
+			      	<ul class="dropdown-menu">
+                <li class="active"><a href="admin.php">Admin Dashboard</a></li>
+			      	</ul>
+            </li>
+            <?php } ?>
         </ul>
         <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
     </div>
