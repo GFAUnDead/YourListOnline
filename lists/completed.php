75b242508eb99f9b55490caf7ef000305f7bc72d
GFAUnDead <contact@gfaundead.stream>
2023-05-10 08:01:48 +1000

    Update completed.php

--- lists/completed.php
+++ lists/completed.php
@@ -81,7 +81,13 @@ if (isset($_POST['task_id'])) {
             <li><a href="dashboard.php">Dashboard</a></li>
             <li><a href="insert.php">Add</a></li>
             <li class="active"><a href="completed.php">Completed</a></li>
-            <li><a href="update_objective.php">Update</a></li>
+            <li class="dropdown dropdown-hover">
+                <a class="dropdown" data-toggle="dropdown">Update <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="update_objective.php">Update Objective</a></li>
+                    <li><a href="update_category.php">Update Category</a></li>
+                </ul>
+            </li>
             <li><a href="remove.php">Remove</a></li>
             <li class="dropdown dropdown-hover">
                 <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
