d0b6e04a1c7fcdd1095390a1638bdbbc032766f7
GFAUnDead <contact@gfaundead.stream>
2023-05-10 08:06:52 +1000

    Update profile.php

--- discord/profile.php
+++ discord/profile.php
@@ -73,7 +73,13 @@ if(!isset($_SESSION["access_token"]) || $_SESSION["access_token"] !== true){
             <li><a href="dashboard.php">Dashboard</a></li>
             <li><a href="insert.php">Add</a></li>
             <li><a href="completed.php">Completed</a></li>
-            <li><a href="update.php">Update</a></li>
+            <li class="dropdown dropdown-hover">
+                <a class="dropdown" data-toggle="dropdown">Update <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="update_objective.php">Update Objective</a></li>
+                    <li><a href="update_category.php">Update Category</a></li>
+                </ul>
+            </li>
             <li><a href="remove.php">Remove</a></li>
             <li class="dropdown dropdown-hover">
                 <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
@@ -87,8 +93,17 @@ if(!isset($_SESSION["access_token"]) || $_SESSION["access_token"] !== true){
 			      	<ul class="dropdown-menu">
 			      		<li class="active"><a href="profile.php">View Profile</a></li>
 			      		<li><a href="update_profile.php">Update Profile</a></li>
+                        <li><a href="logout.php">Logout</a></li>
 			      	</ul>
             </li>
+            <?php if ($_SESSION['is_admin']) { ?>
+            <li class="dropdown dropdown-hover">
+			      <a class="dropdown" data-toggle="dropdown">Admins <span class="caret"></span></a>
+			      	<ul class="dropdown-menu">
+                <li><a href="admin.php">Admin Dashboard</a></li>
+			      	</ul>
+            </li>
+            <?php } ?>
         </ul>
         <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
     </div>
