3183ebd01dab075b49ec16a949a5ac357640eeae
GFAUnDead <contact@gfaundead.stream>
2023-05-12 14:29:23 +1000

    Update obs_options.php

--- lists/obs_options.php
+++ lists/obs_options.php
@@ -61,7 +61,7 @@ $colour = isset($settings['colour']) ? $settings['colour'] : null;
         <ul class="nav navbar-nav">
             <li><a href="dashboard.php">Dashboard</a></li>
             <li><a href="insert.php">Add</a></li>
-            <li><a href="completed.php">Completed</a></li>
+            <li><a href="remove.php">Remove</a></li>
             <li class="dropdown dropdown-hover">
                 <a class="dropdown" data-toggle="dropdown">Update <span class="caret"></span></a>
                 <ul class="dropdown-menu">
@@ -69,7 +69,7 @@ $colour = isset($settings['colour']) ? $settings['colour'] : null;
                     <li><a href="update_category.php">Update Category</a></li>
                 </ul>
             </li>
-            <li><a href="remove.php">Remove</a></li>
+            <li><a href="completed.php">Completed</a></li>
             <li class="dropdown dropdown-hover">
                 <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
                 <ul class="dropdown-menu">
