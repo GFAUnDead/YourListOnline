7a27557c68f7dd93619d95b172beb7cd2c514baf
GFAUnDead <contact@gfaundead.stream>
2023-05-12 14:29:22 +1000

    Update insert.php

--- lists/insert.php
+++ lists/insert.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <ul class="nav navbar-nav">
             <li><a href="dashboard.php">Dashboard</a></li>
             <li class="active"><a href="insert.php">Add</a></li>
-            <li><a href="completed.php">Completed</a></li>
+            <li><a href="remove.php">Remove</a></li>
             <li class="dropdown dropdown-hover">
                 <a class="dropdown" data-toggle="dropdown">Update <span class="caret"></span></a>
                 <ul class="dropdown-menu">
@@ -70,7 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <li><a href="update_category.php">Update Category</a></li>
                 </ul>
             </li>
-            <li><a href="remove.php">Remove</a></li>
+            <li><a href="completed.php">Completed</a></li>
             <li class="dropdown dropdown-hover">
                 <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
                 <ul class="dropdown-menu">
