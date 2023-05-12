2026a761b34b888b021f292b2dc9437b181761d1
GFAUnDead <contact@gfaundead.stream>
2023-05-12 14:19:26 +1000

    Update dashboard.php

--- lists/dashboard.php
+++ lists/dashboard.php
@@ -98,6 +98,7 @@ if (!$result) {
 			      	<ul class="dropdown-menu">
 			      		<li><a href="profile.php">View Profile</a></li>
 			      		<li><a href="update_profile.php">Update Profile</a></li>
+                <li><a href="obs_options.php">OBS Viewing Options</a></li>
                 <li><a href="logout.php">Logout</a></li>
 			      	</ul>
             </li>
