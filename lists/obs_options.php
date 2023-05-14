12d4133fc22288b273fb091cb39c4f95997d8a84
SimDifRandomHD <contact@gfaundead.stream>
2023-05-14 19:18:29 +1000

    Update obs_options.php

--- lists/obs_options.php
+++ lists/obs_options.php
@@ -14,18 +14,19 @@ if (!isset($_SESSION['loggedin'])) {
 // Fetch the user's data from the database
 $user_id = $_SESSION['user_id'];
 
-// Retrieve font, color, list, and shadow data for the user from the showobs table
+// Retrieve font, color, list, shadow, and bold data for the user from the showobs table
 $stmt = $conn->prepare("SELECT * FROM showobs WHERE user_id = ?");
 $stmt->bind_param("i", $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
 $settings = $result->fetch_assoc();
 
-// Retrieve font, color, list, and shadow data for the user from the showobs table
+// Retrieve font, color, list, shadow, and bold data for the user from the showobs table
 $font = isset($settings['font']) && $settings['font'] !== '' ? $settings['font'] : 'Not set';
 $color = isset($settings['color']) && $settings['color'] !== '' ? $settings['color'] : 'Not set';
 $list = isset($settings['list']) && $settings['list'] !== '' ? $settings['list'] : 'Bullet';
 $shadow = isset($settings['shadow']) && $settings['shadow'] == 1 ? true : false;
+$bold = isset($settings['bold']) && $settings['bold'] == 1 ? true : false;
 
 // Process form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -34,12 +35,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $selectedColor = isset($_POST["color"]) ? $_POST["color"] : '';
     $selectedList = isset($_POST["list"]) ? $_POST["list"] : 'Bullet';
     $selectedShadow = isset($_POST["shadow"]) ? 1 : 0;
+    $selectedBold = isset($_POST["bold"]) ? 1 : 0;
 
     // Check if the user has existing settings
     if ($result->num_rows > 0) {
-        // Update the font, color, list, and shadow data in the database
-        $stmt = $conn->prepare("UPDATE showobs SET font = ?, color = ?, list = ?, shadow = ? WHERE user_id = ?");
-        $stmt->bind_param("sssii", $selectedFont, $selectedColor, $selectedList, $selectedShadow, $user_id);
+        // Update the font, color, list, shadow, and bold data in the database
+        $stmt = $conn->prepare("UPDATE showobs SET font = ?, color = ?, list = ?, shadow = ?, bold = ? WHERE user_id = ?");
+        $stmt->bind_param("sssiii", $selectedFont, $selectedColor, $selectedList, $selectedShadow, $selectedBold, $user_id);
         if ($stmt->execute()) {
             // Update successful
             header("Location: obs_options.php");
@@ -49,8 +51,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     } else {
         // Insert new settings for the user
-        $stmt = $conn->prepare("INSERT INTO showobs (user_id, font, color, list, shadow) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("isssi", $user_id, $selectedFont, $selectedColor, $selectedList, $selectedShadow);
+        $stmt = $conn->prepare("INSERT INTO showobs (user_id, font, color, list, shadow, bold) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("isssii", $user_id, $selectedFont, $selectedColor, $selectedList, $selectedShadow, $selectedBold);
         if ($stmt->execute()) {
             // Insertion successful
             header("Location: obs_options.php");
@@ -139,10 +141,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <h1>Font & Color Settings:</h1>
     <h3>Your Current Settings:</h3>
     <?php if ($font !== '' || $color !== '') { ?>
-        <p>Your selected font is: <?php echo $font ?></p>
-        <p>Your selected color is: <?php echo $color ?></p>
-        <p>Your selected list type is: <?php echo $list ?></p>
-        <p>Text shadow: <?php echo $shadow ? 'Enabled' : 'Disabled'; ?></p>
+        <table class="table">
+            <tr>
+                <th>Setting</th>
+                <th>Value</th>
+            </tr>
+            <tr>
+                <td>Font</td>
+                <td><?php echo $font ?></td>
+            </tr>
+            <tr>
+                <td>Color</td>
+                <td><?php echo $color ?></td>
+            </tr>
+            <tr>
+                <td>List Type</td>
+                <td><?php echo $list ?></td>
+            </tr>
+            <tr>
+                <td>Text Shadow</td>
+                <td><?php echo $shadow ? 'Enabled' : 'Disabled'; ?></td>
+            </tr>
+            <tr>
+                <td>Text Bold</td>
+                <td><?php echo $bold ? 'Enabled' : 'Disabled'; ?></td>
+            </tr>
+        </table>
     <?php } else { ?>
         <p>No font and color settings have been set.</p>
     <?php } ?>
@@ -180,6 +204,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="form-group">
             <label for="shadow">Text Shadow:</label>
             <input type="checkbox" name="shadow" value="1" <?php if ($shadow) echo 'checked'; ?>>
+        </div>
+        <div class="form-group">
+            <label for="bold">Text Bold:</label>
+            <input type="checkbox" name="bold" value="1" <?php if ($bold) echo 'checked'; ?>>
         </div>
         <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
         <input type="submit" value="Save" class="btn btn-primary">
