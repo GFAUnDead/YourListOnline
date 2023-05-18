1a016c0b05276d5e4eff4345757b334a51fafb44
SimDifRandomHD <contact@gfaundead.stream>
2023-05-19 05:28:22 +1000

    Update dashboard.php

--- lists/admins/dashboard.php
+++ lists/admins/dashboard.php
@@ -50,13 +50,13 @@ if ($_SESSION['is_admin'] == 1) {
 <html>
 <head>
   <title>YourListOnline - Admin Dashboard</title>
-  <link rel="icon" href="img/logo.png" type="image/png" />
-  <link rel="apple-touch-icon" href="img/logo.png">
+  <link rel="icon" href="../img/logo.png" type="image/png" />
+  <link rel="apple-touch-icon" href="../img/logo.png">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="css/style.css">
-  <script src="js/about.js"></script>
+  <link rel="stylesheet" href="../css/style.css">
+  <script src="../js/about.js"></script>
   <style type="text/css">
     body {
       font: 14px sans-serif;
