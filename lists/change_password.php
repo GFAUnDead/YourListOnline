817b68f05f6545b95ad15933e0475bc067783ba8
SimDifRandomHD <contact@gfaundead.stream>
2023-05-22 05:17:49 +1000

    Update change_password.php

--- lists/change_password.php
+++ lists/change_password.php
@@ -92,8 +92,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <html>
 <head>
     <title>Change Password - YourListOnline</title>
-    <link rel="icon" href="img/logo.png" type="image/png" />
-    <link rel="apple-touch-icon" href="img/logo.png">
+    <link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
+    <link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
