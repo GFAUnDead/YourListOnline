d0f6a8d0f9585621be6ccf1ce84870cc324fee05
SimDifRandomHD <contact@gfaundead.stream>
2023-05-23 22:25:26 +1000

    Update obs_options.php

--- lists/obs_options.php
+++ lists/obs_options.php
@@ -74,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <link rel="stylesheet" href="css/style.css">
-  <script src="js/about.js"></script>
+  <script src="https://yourlist.online/js/about.js"></script>
   <script src="js/obsbutton.js"></script>
   <script src="js/profile.js"></script>
   <style type="text/css">
