bccb39925e1cf2d031b0a6307623bbbde4aa60f9
SimDifRandomHD <contact@gfaundead.stream>
2023-05-23 22:10:17 +1000

    Update dashboard.php

--- lists/admins/dashboard.php
+++ lists/admins/dashboard.php
@@ -31,8 +31,19 @@ $_SESSION['is_admin'] = $user_data['is_admin'];
 
 // Check if the user is an admin
 if ($_SESSION['is_admin'] == 1) {
-  // The user is an admin, load the page
-  $sql = "SELECT todos.*, users.username FROM todos INNER JOIN users ON todos.user_id = users.id ORDER BY id ASC";
+  // Get the category filter value if set
+  $categoryFilter = $_GET['categoryFilter'] ?? 'all';
+
+  // Build the SQL query based on the category filter
+  $sql = "SELECT todos.*, users.username FROM todos INNER JOIN users ON todos.user_id = users.id";
+
+  if ($categoryFilter !== 'all') {
+    // Add a WHERE condition to filter by category
+    $sql .= " INNER JOIN categories ON todos.category = categories.id WHERE categories.id = '$categoryFilter'";
+  }
+
+  $sql .= " ORDER BY todos.id ASC";
+  
   $result = mysqli_query($conn, $sql);
 
   // Handle errors
@@ -117,9 +128,29 @@ if ($_SESSION['is_admin'] == 1) {
         <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
     </div>
 </nav>
-    <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
+    <h1>Welcome, <?php echo $_SESSION['username']; ?>, to the admin dashboard!</h1>
+    
+    <!-- Category filter dropdown -->
+    <div class="category-filter">
+      <label for="categoryFilter">Filter by Category:</label>
+      <select id="categoryFilter" onchange="applyCategoryFilter()">
+        <option value="all" <?php if ($categoryFilter === 'all') echo 'selected'; ?>>All</option>
+        <?php
+          $categories_sql = "SELECT id, category FROM categories";
+          $categories_result = mysqli_query($conn, $categories_sql);
+
+          while ($category_row = mysqli_fetch_assoc($categories_result)) {
+            $categoryId = $category_row['id'];
+            $categoryName = $category_row['category'];
+            $selected = ($categoryFilter == $categoryId) ? 'selected' : '';
+            echo "<option value=\"$categoryId\" $selected>$categoryName</option>";
+          }
+        ?>
+      </select>
+    </div>
+
     <h2>Your Current List:</h2>
-    <?php echo "Number of rows in your list: " . mysqli_num_rows($result); ?>    
+    <?php echo "Number of total tasks in the category: " . mysqli_num_rows($result); ?>    
     <table class="table">
       <thead>
         <tr>
@@ -152,5 +183,14 @@ if ($_SESSION['is_admin'] == 1) {
         <?php endwhile; ?>
       </tbody>
     </table>
+
+    <script>
+      function applyCategoryFilter() {
+        const categoryFilter = document.getElementById('categoryFilter').value;
+        const currentUrl = new URL(window.location.href);
+        currentUrl.searchParams.set('categoryFilter', categoryFilter);
+        window.location.href = currentUrl.toString();
+      }
+    </script>
 </body>
 </html>
