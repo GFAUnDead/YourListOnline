8d881f0193da2f3e946fed615dadfe7787b687db
GFAUnDead <contact@gfaundead.stream>
2023-05-24 06:37:32 +1000

    Update profile.php

--- lists/profile.php
+++ lists/profile.php
@@ -45,10 +45,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <link rel="stylesheet" href="css/style.css">
-  <script src="https://yourlist.online/js/about.js"></script>
-  <script src="js/obsbutton.js"></script>
-  <script src="js/profile.js"></script>
+  <link rel="stylesheet" href="https://cdn.yourlist.online/css/list.css">
+  <script src="https://cdn.yourlist.online/js/about.js"></script>
+  <script src="https://cdn.yourlist.online/js/obsbutton.js"></script>
+  <script src="https://cdn.yourlist.online/js/profile.js"></script>
   <style type="text/css">
     body {
       font: 14px sans-serif;
