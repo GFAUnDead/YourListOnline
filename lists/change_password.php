ad4f051688a591035fc53d0abb48650f529616eb
SimDifRandomHD <contact@gfaundead.stream>
2023-05-23 22:25:20 +1000

    Update change_password.php

--- lists/change_password.php
+++ lists/change_password.php
@@ -99,7 +99,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="js/about.js"></script>
+    <script src="https://yourlist.online/js/about.js"></script>
     <style type="text/css">
       a.popup-link {
         text-decoration: none;
