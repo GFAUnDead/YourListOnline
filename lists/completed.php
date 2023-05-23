b7eeabb7ac4af31aee03cb7f4d0f373bd78383d7
GFAUnDead <contact@gfaundead.stream>
2023-05-24 06:36:18 +1000

    Update completed.php

--- lists/completed.php
+++ lists/completed.php
@@ -61,8 +61,8 @@ if (isset($_POST['task_id'])) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://yourlist.online/js/about.js"></script>
+    <link rel="stylesheet" href="https://cdn.yourlist.online/css/list.css">
+  <script src="https://cdn.yourlist.online/js/about.js"></script>
     <style type="text/css">
       body {
         font: 14px sans-serif;
