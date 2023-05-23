682845a65ae0ba64304b762673e86952b97cf195
GFAUnDead <contact@gfaundead.stream>
2023-05-24 06:36:23 +1000

    Update insert.php

--- lists/insert.php
+++ lists/insert.php
@@ -36,20 +36,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="css/insert.css">
-    <script src="https://yourlist.online/js/about.js"></script>
-    <style type="text/css">
-      body {
-        font: 14px sans-serif;
-      }
-      .wrapper {
-        width: 350px; padding: 20px;
-      }
-      a.popup-link {
-        text-decoration: none;
-        color: black;
-        cursor: pointer;
-      }
+  <link rel="stylesheet" href="https://cdn.yourlist.online/css/insert.css">
+  <script src="https://cdn.yourlist.online/js/about.js"></script>
+  <style type="text/css">
+    body {
+      font: 14px sans-serif;
+    }
+    .wrapper {
+      width: 350px; padding: 20px;
+    }
+    a.popup-link {
+      text-decoration: none;
+      color: black;
+      cursor: pointer;
+    }
     </style>
 </head>
 <body>
