beec6e44955a6dfcd48bb3bee007d0bd4851cfb2
GFAUnDead <contact@gfaundead.stream>
2023-05-24 13:17:59 +1000

    Update update_profile.php

--- lists/update_profile.php
+++ lists/update_profile.php
@@ -65,16 +65,15 @@ $conn->close();
 <html>
 <head>
     <title>YourListOnline - Update Profile</title>
-    <link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
-    <link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.yourlist.online/css/list.css">
-    <script src="https://cdn.yourlist.online/js/about.js"></script>
-    <script src="js/obsbutton.js"></script>
-    <script src="js/profile.js"></script>
+  <link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
+  <link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.yourlist.online/css/list.css">
+  <script src="https://cdn.yourlist.online/js/about.js"></script>
+    <script src="https://cdn.yourlist.online/js/obsbutton.js"></script>
+    <script src="https://cdn.yourlist.online/js/profile.js"></script>
     <style type="text/css">
         body {
             font: 14px sans-serif;
