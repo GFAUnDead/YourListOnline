65e3c966fefb0b12b586d8b2616ef836fc5a7dba
GFAUnDead <contact@gfaundead.stream>
2023-05-24 13:17:04 +1000

    Update change_password.php

--- lists/change_password.php
+++ lists/change_password.php
@@ -94,11 +94,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <title>Change Password - YourListOnline</title>
     <link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
     <link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.yourlist.online/css/list.css">
     <script src="https://cdn.yourlist.online/js/about.js"></script>
     <style type="text/css">
