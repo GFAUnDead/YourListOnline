ce40b92d41b22b9d4587eb3bf5effb27fcf88464
GFAUnDead <contact@gfaundead.stream>
2023-05-24 13:17:12 +1000

    Update login.php

--- lists/login.php
+++ lists/login.php
@@ -105,22 +105,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
     <link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.yourlist.online/css/list.css">
     <script src="https://cdn.yourlist.online/js/about.js"></script>
     <style type="text/css">
-      body {
-        font: 14px sans-serif;
-      }
-      .wrapper {
-        width: 350px; padding: 20px;
-      }
-      a.popup-link {
-        text-decoration: none;
-        color: black;
-        cursor: pointer;
-      }
+        body {
+            font: 14px sans-serif;
+        }
+        .wrapper {
+            width: 350px; padding: 20px;
+        }
+            a.popup-link {
+            text-decoration: none;
+            color: black;
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
