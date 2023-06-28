0bc32f619dc294e4a362b1b2f02d943aca5d4c44
GFAUnDead <contact@gfaundead.stream>
2023-06-28 11:28:48 +1000

    Update add_category.php

--- twitch/add_category.php
+++ twitch/add_category.php
@@ -21,6 +21,7 @@ $result = $stmt->get_result();
 $user = $result->fetch_assoc();
 $user_id = $user['id'];
 $username = $user['username'];
+$is_admin = ($user['admin'] == 1);
 
 // Initialize variables
 $category = "";
@@ -153,7 +154,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <li><a href="logout.php">Logout</a></li>
 			      	</ul>
             </li>
-            <?php if ($_SESSION['is_admin']) { ?>
+            <?php if ($is_admin) { ?>
             <li class="dropdown dropdown-hover">
 			      <a class="dropdown" data-toggle="dropdown">Admins <span class="caret"></span></a>
 			      	<ul class="dropdown-menu">
