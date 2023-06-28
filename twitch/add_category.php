45c7d31eaf55b09b01ce9e536228ffeefe5953c3
GFAUnDead <contact@gfaundead.stream>
2023-06-28 12:08:36 +1000

    Update add_category.php

--- twitch/add_category.php
+++ twitch/add_category.php
@@ -21,7 +21,7 @@ $result = $stmt->get_result();
 $user = $result->fetch_assoc();
 $user_id = $user['id'];
 $username = $user['username'];
-$is_admin = ($user['admin'] == 1);
+$is_admin = ($user['is_admin'] == 1);
 
 // Initialize variables
 $category = "";
