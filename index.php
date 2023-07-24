d98ded008634f2b83272d7de2cf005ee35165bf6
gfaUnDead <contact@gfaundead.stream>
2023-07-24 20:02:17 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>YourListOnline - HOME</title>
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>YourListOnline - HOME</title>
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
+    <script src="https://cdn.yourlist.online/js/about.js"></script>
   	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
   	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  	<link rel="stylesheet" href="https://cdn.yourlist.online/css/home.css">
-  	<script src="https://cdn.yourlist.online/js/about.js"></script>
-	<style type="text/css">
-    	a.popup-link {
-    	  text-decoration: none;
-    	  color: black;
-    	  cursor: pointer;
-   		}
-  	</style>
-</head>
-<body>
-	<header>
-		<h1>Welcome to Your List Online</h1>
-	</header>
-	<main>
-		<p>With this website you can keep track of all the tasks you need to complete.</p>
-		<p>To get started, simply log in with your account or use your Twitch or Discord account.</p>
-		<a href="https://access.yourlist.online/dashboard.php"><button class="button">Login</button></a>
-		<a href="https://twitch.yourlist.online/dashboard.php"><button class="twitch-button">Login with Twitch</button></a>
-		<a href=""><button class="discord-button">Login with Discord (COMING SOON)</button></a>
-		<br><br>
-		<p><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
-	</main>
-</body>
+  </head>
+  <body>
+
+<!-- Navigation -->
+<div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="menu">
+      <li class="menu-text">YourListOnline</li>
+      <!--<li><a href="#">One</a></li>-->
+      <!--<li><a href="#">Two</a></li>-->
+      <!--<li><a href="#">Three</a></li>-->
+    </ul>
+  </div><!--
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><a href="#">My Account</a></li>
+      <li><a class="button">Login</a></li>
+    </ul>
+  </div>-->
+</div>
+<!-- /Navigation -->
+
+    <div class="row column text-center">
+      <h2>Welcome to Your List Online</h2>
+      <hr>
+      <p>With this website you can keep track of all the tasks you need to complete.</p>
+		  <p>To get started, simply log in with your account or use your Twitch or Discord account.</p>
+		  <a href="https://access.yourlist.online/dashboard.php"><button class="defult-button">Login</button></a>
+		  <a href="https://twitch.yourlist.online/dashboard.php"><button class="twitch-button">Login with Twitch</button></a>
+		  <a href=""><button class="discord-button">Login with Discord (COMING SOON)</button></a>
+		  <br><br>
+		  <p><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></p>
+    </div>
+    <div class="row small-up-2 large-up-4">
+     
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
 </html>
