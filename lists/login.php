b1d88458010ccfd9490a4f17df797f4b6e91c458
gfaUnDead <contact@gfaundead.stream>
2023-07-24 21:26:26 +1000

    Update login.php

--- lists/login.php
+++ lists/login.php
@@ -102,7 +102,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>YourListOnline - HOME</title>
+    <title>YourListOnline - Login</title>
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
     <script src="https://cdn.yourlist.online/js/about.js"></script>
