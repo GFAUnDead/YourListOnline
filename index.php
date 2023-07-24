b68590e4f15c042f0a5580fa94e35d03c1abadcc
gfaUnDead <contact@gfaundead.stream>
2023-07-24 20:07:38 +1000

    Update index.php

--- index.php
+++ index.php
@@ -17,17 +17,8 @@
   <div class="top-bar-left">
     <ul class="menu">
       <li class="menu-text">YourListOnline</li>
-      <!--<li><a href="#">One</a></li>-->
-      <!--<li><a href="#">Two</a></li>-->
-      <!--<li><a href="#">Three</a></li>-->
     </ul>
-  </div><!--
-  <div class="top-bar-right">
-    <ul class="menu">
-      <li><a href="#">My Account</a></li>
-      <li><a class="button">Login</a></li>
-    </ul>
-  </div>-->
+  </div>
 </div>
 <!-- /Navigation -->
 
