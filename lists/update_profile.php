beb9f0f9a8f2139bc093f9449f8f76bc200c8c82
GFAUnDead <contact@gfaundead.stream>
2023-07-28 14:19:58 +1000

    Update update_profile.php

--- lists/update_profile.php
+++ lists/update_profile.php
@@ -117,10 +117,10 @@ $conn->close();
       <li>
         <a>Profile</a>
         <ul class="vertical menu" data-dropdown-menu>
-			<li><a href="profile.php">View Profile</a></li>
+			  <li><a href="profile.php">View Profile</a></li>
 		    <li class="is-active"><a href="update_profile.php">Update Profile</a></li>
-            <li><a href="obs_options.php">OBS Viewing Options</a></li>
-            <li><a href="logout.php">Logout</a></li>
+        <li><a href="obs_options.php">OBS Viewing Options</a></li>
+        <li><a href="logout.php">Logout</a></li>
         </ul>
       </li>
       <?php if ($_SESSION['is_admin']) { ?>
