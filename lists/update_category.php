a5ef67f605f8b63d9981c51572c63ef9935632aa
GFAUnDead <contact@gfaundead.stream>
2023-07-28 12:00:41 +1000

    Update update_category.php

--- lists/update_category.php
+++ lists/update_category.php
@@ -10,6 +10,9 @@ if (!isset($_SESSION['loggedin'])) {
 
 // Require database connection
 require_once "db_connect.php";
+// Fetch the user's data from the database
+$user_id = $_SESSION['user_id'];
+$username = $_SESSION['username'];
 
 // Get the current hour in 24-hour format (0-23)
 $currentHour = date('G');
