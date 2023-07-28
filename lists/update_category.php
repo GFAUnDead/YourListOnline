3dfba49a36cd65d68609a9ab4e939619e6cdd798
GFAUnDead <contact@gfaundead.stream>
2023-07-28 11:52:38 +1000

    Update update_category.php

--- lists/update_category.php
+++ lists/update_category.php
@@ -11,6 +11,17 @@ if (!isset($_SESSION['loggedin'])) {
 // Require database connection
 require_once "db_connect.php";
 
+// Get the current hour in 24-hour format (0-23)
+$currentHour = date('G');
+// Initialize the greeting variable
+$greeting = '';
+// Check if it's before 12 PM (noon)
+if ($currentHour < 12) {
+    $greeting = "Good morning";
+} else {
+    $greeting = "Good afternoon";
+}
+
 // Get user's to-do list
 $user_id = $_SESSION['user_id'];
 $sql = "SELECT * FROM todos WHERE user_id = $user_id ORDER BY id DESC";
