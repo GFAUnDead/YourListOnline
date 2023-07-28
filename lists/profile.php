303ab237339925a3df6d39d1818d9afdb21c3473
GFAUnDead <contact@gfaundead.stream>
2023-07-28 14:15:08 +1000

    Update profile.php

--- lists/profile.php
+++ lists/profile.php
@@ -52,93 +52,95 @@ if($stmt = $conn->prepare($sql)){
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>YourListOnline - Profile</title>
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
-    <script src="https://cdn.yourlist.online/js/about.js"></script>
-    <script src="https://cdn.yourlist.online/js/obsbutton.js"></script>
-    <script src="https://cdn.yourlist.online/js/profile.js"></script>
-  	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
-  	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
-  </head>
+    <link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
+    <link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
+</head>
 <body>
 <!-- Navigation -->
 <div class="title-bar" data-responsive-toggle="mobile-menu" data-hide-for="medium">
-  <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
-  <div class="title-bar-title">Menu</div>
+    <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+    <div class="title-bar-title">Menu</div>
 </div>
 <nav class="top-bar stacked-for-medium" id="mobile-menu">
-  <div class="top-bar-left">
-    <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
-      <li class="menu-text">YourListOnline</li>
-      <li><a href="dashboard.php">Dashboard</a></li>
-      <li><a href="insert.php">Add</a></li>
-      <li><a href="remove.php">Remove</a></li>
-      <li>
-        <a>Update</a>
-        <ul class="vertical menu" data-dropdown-menu>
-          <li><a href="update_objective.php">Update Objective</a></li>
-          <li><a href="update_category.php">Update Objective Category</a></li>
+    <div class="top-bar-left">
+        <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+            <li class="menu-text">YourListOnline</li>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="insert.php">Add</a></li>
+            <li><a href="remove.php">Remove</a></li>
+            <li>
+                <a>Update</a>
+                <ul class="vertical menu" data-dropdown-menu>
+                    <li><a href="update_objective.php">Update Objective</a></li>
+                    <li><a href="update_category.php">Update Objective Category</a></li>
+                </ul>
+            </li>
+            <li><a href="completed.php">Completed</a></li>
+            <li>
+                <a>Categories</a>
+                <ul class="vertical menu" data-dropdown-menu>
+                    <li><a href="categories.php">View Categories</a></li>
+                    <li><a href="add_category.php">Add Category</a></li>
+                </ul>
+            </li>
+            <li>
+                <a>Profile</a>
+                <ul class="vertical menu" data-dropdown-menu>
+                    <li><a href="profile.php">View Profile</a></li>
+                    <li class="is-active"><a href="update_profile.php">Update Profile</a></li>
+                    <li><a href="obs_options.php">OBS Viewing Options</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </li>
+            <?php if ($_SESSION['is_admin']) { ?>
+            <li>
+                <a>Admins</a>
+                <ul class="vertical menu" data-dropdown-menu>
+                    <li><a href="../admins/dashboard.php" target="_self">Admin Dashboard</a></li>
+                </ul>
+            </li>
+            <?php } ?>
         </ul>
-      </li>
-      <li><a href="completed.php">Completed</a></li>
-      <li>
-        <a>Categories</a>
-        <ul class="vertical menu" data-dropdown-menu>
-          <li><a href="categories.php">View Categories</a></li>
-          <li><a href="add_category.php">Add Category</a></li>
+    </div>
+    <div class="top-bar-right">
+        <ul class="menu">
+            <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
         </ul>
-      </li>
-      <li>
-        <a>Profile</a>
-        <ul class="vertical menu" data-dropdown-menu>
-			<li><a href="profile.php">View Profile</a></li>
-		    <li class="is-active"><a href="update_profile.php">Update Profile</a></li>
-            <li><a href="obs_options.php">OBS Viewing Options</a></li>
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-      </li>
-      <?php if ($_SESSION['is_admin']) { ?>
-        <li>
-        <a>Admins</a>
-        <ul class="vertical menu" data-dropdown-menu>
-					<li><a href="../admins/dashboard.php" target="_self">Admin Dashboard</a></li>
-        </ul>
-      </li>
-      <?php } ?>
-    </ul>
-  </div>
-  <div class="top-bar-right">
-    <ul class="menu">
-      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
-    </ul>
-  </div>
+    </div>
 </nav>
 <!-- /Navigation -->
-
 <div class="row column">
-<br>
-<h1><?php echo "<h1>$greeting, $username!</h1>"; ?></h1>
-<h2>Your Profile</h2>
-<img src="<?php echo $twitch_profile_image_url; ?>" width="150px" height="150px" alt="Twitch Profile Image for <?php echo $username; ?>">
-<br><br>
-<p><strong>Your Username:</strong> <?php echo $_SESSION['username']; ?></p>
-<p><strong>You Joined:</strong> <?php echo date('F j, Y', strtotime($_SESSION['signup_date'])); ?> (AET)</p>
-<p><strong>Your Last Login:</strong> <?php echo date('F j, Y', strtotime($_SESSION['last_login'])); ?> at <?php echo date('g:i A', strtotime($last_login)); ?> (AET)</p>
-<p><strong>Your API Key:</strong> <span class="api-key-wrapper" style="display: none;"><?php echo $api_key; ?></span></p>
-<button type="button" class="defult-button" id="show-api-key">Show API Key</button>
-<button type="button" class="defult-button" id="hide-api-key" style="display:none;">Hide API Key</button>
-<br><br>
-<button class="defult-button" onclick="showOBSInfo()">HOW TO PUT ON YOUR STREAM</button>
-<br><br>
-<?php if ($_SESSION['is_admin']) { ?><a href="change_password.php" class="defult-button">Change Password</a><br><br><?php } ?>
-<a href="logout.php" class="logout-button">Logout</a>
-</div>
-<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+    <br>
+    <h1><?php echo "<h1>$greeting, $username!</h1>"; ?></h1>
+    <h2>Your Profile</h2>
+    <img src="<?php echo $twitch_profile_image_url; ?>" width="150px" height="150px" alt="Twitch Profile Image for <?php echo $username; ?>">
+    <br><br>
+    <p><strong>Your Username:</strong> <?php echo $_SESSION['username']; ?></p>
+    <p><strong>You Joined:</strong> <?php echo date('F j, Y', strtotime($_SESSION['signup_date'])); ?> (AET)</p>
+    <p><strong>Your Last Login:</strong> <?php echo date('F j, Y', strtotime($_SESSION['last_login'])); ?> at <?php echo date('g:i A', strtotime($last_login)); ?> (AET)</p>
+    <p><strong>Your API Key:</strong> <span class="api-key-wrapper" style="display: none;"><?php echo $_SESSION['api_key']; ?></span></p>
+    <button type="button" class="defult-button" id="show-api-key">Show API Key</button>
+    <button type="button" class="defult-button" id="hide-api-key" style="display:none;">Hide API Key</button>
+    <br><br>
+    <button class="defult-button" onclick="showOBSInfo()">HOW TO PUT ON YOUR STREAM</button>
+    <br><br>
+    <?php if ($_SESSION['is_admin']) { ?><a href="change_password.php" class="defult-button">Change Password</a><br><br><?php } ?>
+    <a href="logout.php" class="logout-button">Logout</a>
+</div
+<!-- Include jQuery first -->
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script
+<!-- Include your profile.js script next -->
+<script src="https://cdn.yourlist.online/js/profile.js"></script>
+<script src="https://cdn.yourlist.online/js/about.js" defer></script>
+<script src="https://cdn.yourlist.online/js/obsbutton.js" defer></script>
+<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script
+<!-- Initialize Foundation at the end -->
 <script>$(document).foundation();</script>
 </body>
 </html>
