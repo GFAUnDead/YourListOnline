fcaf5db7f546c100c6d5d2866eea2f9fcf46123e
GFAUnDead <contact@gfaundead.stream>
2023-07-28 12:00:39 +1000

    Update remove.php

--- lists/remove.php
+++ lists/remove.php
@@ -8,13 +8,16 @@ if (!isset($_SESSION['loggedin'])) {
     exit;
 }
 
-// Connect to database
+// Require database connection
 require_once "db_connect.php";
+// Fetch the user's data from the database
+$user_id = $_SESSION['user_id'];
+$username = $_SESSION['username'];
 
 // Get the current hour in 24-hour format (0-23)
-$currentHour = date('G')
+$currentHour = date('G');
 // Initialize the greeting variable
-$greeting = ''
+$greeting = '';
 // Check if it's before 12 PM (noon)
 if ($currentHour < 12) {
     $greeting = "Good morning";
