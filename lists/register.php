9fd9c169eba73069ee89821030e90aa1cf01fb50
GFAUnDead <contact@gfaundead.stream>
2023-07-28 11:52:33 +1000

    Update register.php

--- lists/register.php
+++ lists/register.php
@@ -5,6 +5,17 @@ session_start();
 // include database connection
 require_once "db_connect.php";
 
+// Get the current hour in 24-hour format (0-23)
+$currentHour = date('G');
+// Initialize the greeting variable
+$greeting = '';
+// Check if it's before 12 PM (noon)
+if ($currentHour < 12) {
+    $greeting = "Good morning";
+} else {
+    $greeting = "Good afternoon";
+}
+
 // define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
