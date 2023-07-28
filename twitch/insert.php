f98816b5db1f8d99e6ffa2da149d3bd08ad46069
gfaUnDead <contact@gfaundead.stream>
2023-07-28 20:16:19 +1000

    Update insert.php

--- twitch/insert.php
+++ twitch/insert.php
@@ -11,6 +11,21 @@ if (!isset($_SESSION['access_token'])) {
 // Connect to database
 require_once "db_connect.php";
 
+// Fetch the user's data from the database
+$user_id = $_SESSION['user_id'];
+$username = $_SESSION['username'];
+
+// Get the current hour in 24-hour format (0-23)
+$currentHour = date('G');
+// Initialize the greeting variable
+$greeting = '';
+// Check if it's before 12 PM (noon)
+if ($currentHour < 12) {
+    $greeting = "Good morning";
+} else {
+    $greeting = "Good afternoon";
+}
+
 // Fetch the user's data from the database based on the access_token
 $access_token = $_SESSION['access_token'];
 
@@ -37,103 +52,113 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-  <title>YourListOnline - Add New Items</title>
-  <link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
-  <link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://cdn.yourlist.online/css/insert.css">
-  <script src="https://cdn.yourlist.online/js/about.js"></script>
-  <style type="text/css">
-    body {
-      font: 14px sans-serif;
-    }
-    .wrapper {
-      width: 350px; padding: 20px;
-    }
-    a.popup-link {
-      text-decoration: none;
-      color: black;
-      cursor: pointer;
-    }
-    </style>
-</head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>YourListOnline - Add Objective</title>
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
+    <script src="https://cdn.yourlist.online/js/about.js"></script>
+  	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
+  	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
+  </head>
 <body>
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="https://yourlist.online/">YourListOnline</a>
-        </div>
-        <ul class="nav navbar-nav">
-            <li><a href="dashboard.php">Dashboard</a></li>
-            <li class="active"><a href="insert.php">Add</a></li>
-            <li><a href="remove.php">Remove</a></li>
-            <li class="dropdown dropdown-hover">
-                <a class="dropdown" data-toggle="dropdown">Update <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="update_objective.php">Update Objective</a></li>
-                    <li><a href="update_category.php">Update Objective Category</a></li>
-                </ul>
-            </li>
-            <li><a href="completed.php">Completed</a></li>
-            <li class="dropdown dropdown-hover">
-                <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="categories.php">View Categories</a></li>
-                    <li><a href="add_category.php">Add Category</a></li>
-                </ul>
-            </li>
-            <li class="dropdown dropdown-hover">
-			      <a class="dropdown" data-toggle="dropdown">Profile <span class="caret"></span></a>
-			      	<ul class="dropdown-menu">
-			      		<li><a href="profile.php">View Profile</a></li>
-			      		<li><a href="update_profile.php">Update Profile</a></li>
-                <li><a href="obs_options.php">OBS Viewing Options</a></li>
-                <li><a href="logout.php">Logout</a></li>
-			      	</ul>
-            </li>
-            <?php if ($is_admin) { ?>
-            <li class="dropdown dropdown-hover">
-			      <a class="dropdown" data-toggle="dropdown">Admins <span class="caret"></span></a>
-			      	<ul class="dropdown-menu">
-                <li><a href="admins/dashboard.php">Admin Dashboard</a></li>
-			      	</ul>
-            </li>
-            <?php } ?>
+<!-- Navigation -->
+<div class="title-bar" data-responsive-toggle="mobile-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+<nav class="top-bar stacked-for-medium" id="mobile-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+      <li class="menu-text">YourListOnline</li>
+      <li><a href="dashboard.php">Dashboard</a></li>
+      <li class="is-active"><a href="insert.php">Add</a></li>
+      <li><a href="remove.php">Remove</a></li>
+      <li>
+        <a>Update</a>
+        <ul class="vertical menu" data-dropdown-menu>
+          <li><a href="update_objective.php">Update Objective</a></li>
+          <li><a href="update_category.php">Update Objective Category</a></li>
         </ul>
-        <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
-    </div>
+      </li>
+      <li><a href="completed.php">Completed</a></li>
+      <li>
+        <a>Categories</a>
+        <ul class="vertical menu" data-dropdown-menu>
+          <li><a href="categories.php">View Categories</a></li>
+          <li><a href="add_category.php">Add Category</a></li>
+        </ul>
+      </li>
+      <li>
+        <a>Profile</a>
+        <ul class="vertical menu" data-dropdown-menu>
+					<li><a href="profile.php">View Profile</a></li>
+					<li><a href="update_profile.php">Update Profile</a></li>
+          <li><a href="obs_options.php">OBS Viewing Options</a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </li>
+      <?php if ($is_admin) { ?>
+        <li>
+        <a>Admins</a>
+        <ul class="vertical menu" data-dropdown-menu>
+					<li><a href="../admins/dashboard.php" target="_self">Admin Dashboard</a></li>
+        </ul>
+      </li>
+      <?php } ?>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
+    </ul>
+  </div>
 </nav>
-  <div class="col-md-6">
-  <h1>Welcome, <?php echo $username; ?>!</h1>
-      <form method="post">
-      <h3>Please enter your task to add it to your list:</h3>
-        <div class="form-group">
-          <textarea id="objective" name="objective" class="form-control"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="category">Category:</label>
-          <select id="category" name="category" class="form-control">
-            <?php
-            // retrieve categories from database
-            $stmt = $conn->prepare("SELECT * FROM categories WHERE user_id = '$user_id' OR user_id IS NULL");
-            $stmt->execute();
-            $result = $stmt->get_result();
-      
-            // display categories as options in dropdown menu
-            while ($row = $result->fetch_assoc()) {
-              echo '<option value="'.$row['id'].'">'.$row['category'].'</option>';
-            }
-            ?>
-          </select>
-        </div>
-        <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
-        <button type="submit" class="btn btn-primary">Add</button>
-        <a href="dashboard.php" class="btn btn-default">Cancel</a>
-      </form>
-    </div>
+<!-- /Navigation -->
+
+<div class="row column">
+<br>
+<h1><?php echo "$greeting, $username!"; ?></h1>
+<br>
+<form method="post">
+<h3>Please enter your task to add it to your list:</h3>
+  <div class="medium-5 large-6 cell">
+    <textarea id="objective" name="objective" class="form-control"></textarea>
+  </div>
+  <div class="medium-5 large-2 cell">
+    <label for="category">Category:</label>
+    <select id="category" name="category" class="form-control">
+      <?php
+      // retrieve categories from database
+      $stmt = $conn->prepare("SELECT * FROM categories WHERE user_id = '$user_id' OR user_id IS NULL");
+      $stmt->execute();
+      $result = $stmt->get_result();
+
+      // display categories as options in dropdown menu
+      while ($row = $result->fetch_assoc()) {
+        echo '<option value="'.$row['id'].'">'.$row['category'].'</option>';
+      }
+      ?>
+    </select>
+  </div>
+  <input type="hidden" name="user_id" value="<?php echo $_SESSION["user_id"]; ?>">
+  <button type="submit" class="save-button">Add</button>
+  <a href="dashboard.php">Cancel</a>
+</form>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>$(document).foundation();</script>
+<script>
+  // JavaScript function to handle the category filter change
+  document.getElementById("categoryFilter").addEventListener("change", function() {
+    var selectedCategoryId = this.value;
+    // Redirect to the page with the selected category filter
+    window.location.href = "insert.php?category=" + selectedCategoryId;
+  });
+</script>
 </body>
 </html>
