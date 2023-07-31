f19fd34c6ec7f9dabd36fa0519c45016f89b3925
gfaUnDead <contact@gfaundead.stream>
2023-07-31 21:26:07 +1000

    Update dashboard.php

--- discord/dashboard.php
+++ discord/dashboard.php
@@ -1,41 +1,43 @@
 <?php
-// Start session
+// Initialize the session
 session_start();
 
-// Check if user is logged in
+// check if user is logged in
 if (!isset($_SESSION['access_token'])) {
-  header("Location: login.php");
-  exit();
+    header('Location: login.php');
+    exit();
 }
 
-// Require database connection
+// Connect to database
 require_once "db_connect.php";
 
-// Fetch the user's data from the database
-$user_id = $_SESSION['user_id'];
-$sql = "SELECT * FROM users WHERE id = '$user_id'";
-$result = mysqli_query($conn, $sql);
-
-// Check if the query succeeded
-if (!$result) {
-  echo "Error: " . mysqli_error($conn);
-  exit();
+// Get the current hour in 24-hour format (0-23)
+$currentHour = date('G');
+// Initialize the greeting variable
+$greeting = '';
+// Check if it's before 12 PM (noon)
+if ($currentHour < 12) {
+    $greeting = "Good morning";
+} else {
+    $greeting = "Good afternoon";
 }
 
-// Get the user's data from the query result
-$user_data = mysqli_fetch_assoc($result);
+// Fetch the user's data from the database based on the access_token
+$access_token = $_SESSION['access_token'];
 
-// Store the user's data in the $_SESSION variable
-$_SESSION['user_data'] = $user_data;
-
-// Set the is_admin flag in the $_SESSION variable
-$_SESSION['is_admin'] = $user_data['is_admin'];
+$stmt = $conn->prepare("SELECT * FROM users WHERE access_token = ?");
+$stmt->bind_param("s", $access_token);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+$user_id = $user['id'];
+$username = $user['username'];
+$is_admin = ($user['is_admin'] == 1);
 
 // Get the selected category filter, default to "all" if not provided
 $categoryFilter = isset($_GET['category']) ? $_GET['category'] : 'all';
 
 // Build the SQL query based on the category filter
-$user_id = $_SESSION['user_id'];
 if ($categoryFilter === 'all') {
   $sql = "SELECT * FROM todos WHERE user_id = '$user_id' ORDER BY id ASC";
 } else {
@@ -52,84 +54,82 @@ if (!$result) {
 }
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-  <title>YourListOnline - Dashboard</title>
-  <link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
-  <link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://cdn.yourlist.online/css/list.css">
-  <script src="https://cdn.yourlist.online/js/about.js"></script>
-  <style type="text/css">
-    body {
-      font: 14px sans-serif;
-    }
-    .wrapper {
-      width: 350px; padding: 20px;
-    }
-    a.popup-link {
-      text-decoration: none;
-      color: black;
-      cursor: pointer;
-    }
-  </style>
-</head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>YourListOnline - Dashboard</title>
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
+    <script src="https://cdn.yourlist.online/js/about.js"></script>
+  	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
+  	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
+  </head>
 <body>
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="https://yourlist.online/">YourListOnline</a>
-        </div>
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="dashboard.php">Dashboard</a></li>
-            <li><a href="insert.php">Add</a></li>
-            <li><a href="remove.php">Remove</a></li>
-            <li class="dropdown dropdown-hover">
-                <a class="dropdown" data-toggle="dropdown">Update <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="update_objective.php">Update Objective</a></li>
-                    <li><a href="update_category.php">Update Objective Category</a></li>
-                </ul>
-            </li>
-            <li><a href="completed.php">Completed</a></li>
-            <li class="dropdown dropdown-hover">
-                <a class="dropdown" data-toggle="dropdown">Categories <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="categories.php">View Categories</a></li>
-                    <li><a href="add_category.php">Add Category</a></li>
-                </ul>
-            </li>
-            <li class="dropdown dropdown-hover">
-			      <a class="dropdown" data-toggle="dropdown">Profile <span class="caret"></span></a>
-			      	<ul class="dropdown-menu">
-			      		<li><a href="profile.php">View Profile</a></li>
-			      		<li><a href="update_profile.php">Update Profile</a></li>
-                <li><a href="obs_options.php">OBS Viewing Options</a></li>
-                <li><a href="logout.php">Logout</a></li>
-			      	</ul>
-            </li>
-            <?php if ($_SESSION['is_admin']) { ?>
-            <li class="dropdown dropdown-hover">
-			      <a class="dropdown" data-toggle="dropdown">Admins <span class="caret"></span></a>
-			      	<ul class="dropdown-menu">
-                <li><a href="admins/dashboard.php">Admin Dashboard</a></li>
-			      	</ul>
-            </li>
-            <?php } ?>
+<!-- Navigation -->
+<div class="title-bar" data-responsive-toggle="mobile-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+<nav class="top-bar stacked-for-medium" id="mobile-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+      <li class="menu-text">YourListOnline</li>
+      <li class="is-active"><a href="dashboard.php">Dashboard</a></li>
+      <li><a href="insert.php">Add</a></li>
+      <li><a href="remove.php">Remove</a></li>
+      <li>
+        <a>Update</a>
+        <ul class="vertical menu" data-dropdown-menu>
+          <li><a href="update_objective.php">Update Objective</a></li>
+          <li><a href="update_category.php">Update Objective Category</a></li>
         </ul>
-        <p class="navbar-text navbar-right"><a class="popup-link" onclick="showPopup()">&copy; <?php echo date("Y"); ?> YourListOnline. All rights reserved.</a></p>
-    </div>
+      </li>
+      <li><a href="completed.php">Completed</a></li>
+      <li>
+        <a>Categories</a>
+        <ul class="vertical menu" data-dropdown-menu>
+          <li><a href="categories.php">View Categories</a></li>
+          <li><a href="add_category.php">Add Category</a></li>
+        </ul>
+      </li>
+      <li>
+        <a>Profile</a>
+        <ul class="vertical menu" data-dropdown-menu>
+					<li><a href="profile.php">View Profile</a></li>
+					<li><a href="update_profile.php">Update Profile</a></li>
+          <li><a href="obs_options.php">OBS Viewing Options</a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </li>
+      <?php if ($is_admin) { ?>
+        <li>
+        <a>Admins</a>
+        <ul class="vertical menu" data-dropdown-menu>
+					<li><a href="../admins/dashboard.php" target="_self">Admin Dashboard</a></li>
+        </ul>
+      </li>
+      <?php } ?>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
+    </ul>
+  </div>
 </nav>
-    <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
+<!-- /Navigation -->
 
-    <!-- Category filter dropdown -->
-    <div class="category-filter">
-      <label for="categoryFilter">Filter by Category:</label>
-      <select id="categoryFilter" onchange="applyCategoryFilter()">
-        <option value="all" <?php if ($categoryFilter === 'all') echo 'selected'; ?>>All</option>
-        <?php
+<div class="row column">
+<br>
+<h1><?php echo "$greeting, $username!"; ?></h1>
+<br>
+<!-- Category Filter Dropdown -->
+<div class="category-filter">
+  <label for="categoryFilter">Filter by Category:</label>
+  <select id="categoryFilter" onchange="applyCategoryFilter()">
+    <option value="all" <?php if ($categoryFilter === 'all') echo 'selected'; ?>>All</option>
+    <?php
           $categories_sql = "SELECT * FROM categories WHERE user_id = '$user_id' OR user_id IS NULL";
           $categories_result = mysqli_query($conn, $categories_sql);
 
@@ -140,42 +140,46 @@ if (!$result) {
             echo "<option value=\"$categoryId\" $selected>$categoryName</option>";
           }
         ?>
-      </select>
-    </div>
+  </select>
+</div>
+<!-- /Category Filter Dropdown -->
 
-    <h2>Your Current List:</h2>
-    <?php echo "Number of total tasks in the category: " . mysqli_num_rows($result); ?>
-    <table class="table">
-      <thead>
+<?php echo "Number of total tasks in the category: " . mysqli_num_rows($result); ?>
+  <table>
+    <thead>
+      <tr>
+        <th>Objective</th>
+        <th width="400">Category</th>
+        <th width="600">Created</th>
+        <th width="600">Last Updated</th>
+        <th width="200">Completed</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php while ($row = mysqli_fetch_assoc($result)): ?>
         <tr>
-          <th>Objective</th>
-          <th>Category</th>
-          <th>Created</th>
-          <th>Last Updated</th>
-          <th>Completed</th>
+          <td><?php echo ($row['completed'] == 'Yes') ? '<s>' . $row['objective'] . '</s>' : $row['objective']; ?></td>
+          <td>
+            <?php
+              $category_id = $row['category'];
+              $category_sql = "SELECT category FROM categories WHERE id = '$category_id'";
+              $category_result = mysqli_query($conn, $category_sql);
+              $category_row = mysqli_fetch_assoc($category_result);
+              echo $category_row['category'];
+            ?>
+          </td>
+          <td><?php echo $row['created_at']; ?></td>
+          <td><?php echo $row['updated_at']; ?></td>
+          <td><?php echo $row['completed']; ?></td>
         </tr>
-      </thead>
-      <tbody>
-        <?php while ($row = mysqli_fetch_assoc($result)): ?>
-          <tr>
-            <td><?php echo ($row['completed'] == 'Yes') ? '<s>' . $row['objective'] . '</s>' : $row['objective']; ?></td>
-            <td>
-              <?php
-                $category_id = $row['category'];
-                $category_sql = "SELECT category FROM categories WHERE id = '$category_id'";
-                $category_result = mysqli_query($conn, $category_sql);
-                $category_row = mysqli_fetch_assoc($category_result);
-                echo $category_row['category'];
-              ?>
-            </td>
-            <td><?php echo $row['created_at']; ?></td>
-            <td><?php echo $row['updated_at']; ?></td>
-            <td><?php echo $row['completed']; ?></td>
-          </tr>
-        <?php endwhile; ?>
-      </tbody>
-    </table>
+      <?php endwhile; ?>
+    </tbody>
+  </table>
+</div>
 
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>$(document).foundation();</script>
 <script>
   // JavaScript function to handle the category filter change
   document.getElementById("categoryFilter").addEventListener("change", function() {
