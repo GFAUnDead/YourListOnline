d31c2e2b54dde116cfd2da35e4efe5a6eaea15ca
GFAUnDead <contact@gfaundead.stream>
2023-08-15 13:01:36 +1000

    Update update_category.php

--- twitch/update_category.php
+++ twitch/update_category.php
@@ -74,6 +74,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
     <script src="https://cdn.yourlist.online/js/about.js"></script>
     <script src="https://cdn.yourlist.online/js/sorttable.js"></script>
+    <script src="https://cdn.yourlist.online/js/darkmode.js"></script>
   	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
   	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
   </head>
@@ -126,7 +127,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   </div>
   <div class="top-bar-right">
     <ul class="menu">
-      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
+      <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button> <a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
     </ul>
   </div>
 </nav>
