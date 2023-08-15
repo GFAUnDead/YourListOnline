3eb84b3e6dd89eedc8493514219da2fae7940638
GFAUnDead <contact@gfaundead.stream>
2023-08-15 13:29:52 +1000

    Update update_profile.php

--- twitch/update_profile.php
+++ twitch/update_profile.php
@@ -131,7 +131,8 @@ $conn->close();
   </div>
   <div class="top-bar-right">
     <ul class="menu">
-      <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button> <a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
+      <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button></li>
+      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
     </ul>
   </div>
 </nav>
