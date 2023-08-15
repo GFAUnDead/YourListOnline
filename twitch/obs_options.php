e904d8857d52f9035bc7c9a5a5792a00b855d5e9
GFAUnDead <contact@gfaundead.stream>
2023-08-15 13:29:58 +1000

    Update obs_options.php

--- twitch/obs_options.php
+++ twitch/obs_options.php
@@ -156,7 +156,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   </div>
   <div class="top-bar-right">
     <ul class="menu">
-      <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button> <a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
+      <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button></li>
+      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
     </ul>
   </div>
 </nav>
