63aaeb82de5795b8a266eacbead03df9d9213af2
GFAUnDead <contact@gfaundead.stream>
2023-08-15 13:01:13 +1000

    Update add_category.php

--- twitch/add_category.php
+++ twitch/add_category.php
@@ -116,6 +116,7 @@ if (empty($category_err)) {
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
     <script src="https://cdn.yourlist.online/js/about.js"></script>
+    <script src="https://cdn.yourlist.online/js/darkmode.js"></script>
   	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
   	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
   </head>
@@ -168,7 +169,7 @@ if (empty($category_err)) {
   </div>
   <div class="top-bar-right">
     <ul class="menu">
-      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
+      <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button> <a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
     </ul>
   </div>
 </nav>
