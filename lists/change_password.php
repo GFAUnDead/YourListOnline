4f49872a4fa2fe3f5bba9596211a5a707fba7592
gfaUnDead <contact@gfaundead.stream>
2023-08-18 23:07:18 +1000

    Update change_password.php

--- lists/change_password.php
+++ lists/change_password.php
@@ -123,7 +123,6 @@ if (empty($current_password_err) && empty($new_password_err) && empty($confirm_p
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
     <script src="https://cdn.yourlist.online/js/about.js"></script>
-    <script src="https://cdn.yourlist.online/js/darkmode.js"></script>
   	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
   	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
   </head>
@@ -177,7 +176,7 @@ if (empty($current_password_err) && empty($new_password_err) && empty($confirm_p
     </div>
     <div class="top-bar-right">
       <ul class="menu">
-        <!--<li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button></li>-->
+        <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button></li>
         <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
       </ul>
     </div>
@@ -207,6 +206,7 @@ if (empty($current_password_err) && empty($new_password_err) && empty($confirm_p
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script src="https://cdn.yourlist.online/js/darkmode.js"></script>
 <script>$(document).foundation();</script>
 <script>
   // JavaScript function to handle the category filter change
