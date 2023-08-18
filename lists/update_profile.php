77fd02076c958c002b14f295ce06af62992e9e0c
gfaUnDead <contact@gfaundead.stream>
2023-08-18 23:07:31 +1000

    Update update_profile.php

--- lists/update_profile.php
+++ lists/update_profile.php
@@ -89,7 +89,6 @@ $conn->close();
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
     <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
     <script src="https://cdn.yourlist.online/js/about.js"></script>
-    <script src="https://cdn.yourlist.online/js/darkmode.js"></script>
   	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
   	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
   </head>
@@ -143,7 +142,7 @@ $conn->close();
   </div>
   <div class="top-bar-right">
     <ul class="menu">
-      <!--<li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button></li>-->
+      <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button></li>
       <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
     </ul>
   </div>
@@ -190,6 +189,7 @@ $conn->close();
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script src="https://cdn.yourlist.online/js/darkmode.js"></script>
 <script>$(document).foundation();</script>
 </body>
 </html>
