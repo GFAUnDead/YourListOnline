1af8c9756e0dadb25bffa8512e773e5512096519
gfaUnDead <contact@gfaundead.stream>
2023-08-21 17:04:07 +1000

    Update change_password.php

--- lists/change_password.php
+++ lists/change_password.php
@@ -135,7 +135,7 @@ if (empty($current_password_err) && empty($new_password_err) && empty($confirm_p
 <nav class="top-bar stacked-for-medium" id="mobile-menu">
     <div class="top-bar-left">
         <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
-            <li class="menu-text">YourListOnline</li>
+            <li class="menu-text menu-text-black">YourListOnline</li>
             <li><a href="dashboard.php">Dashboard</a></li>
             <li><a href="insert.php">Add</a></li>
             <li><a href="remove.php">Remove</a></li>
@@ -183,7 +183,6 @@ if (empty($current_password_err) && empty($new_password_err) && empty($confirm_p
 </nav>
 <!-- /Navigation -->
 
-<div class="dark-mode-content">
 <div class="row column">
 <br>
 <h1><?php echo "$greeting, $username!"; ?></h1>
@@ -217,6 +216,5 @@ if (empty($current_password_err) && empty($new_password_err) && empty($confirm_p
     window.location.href = "dashboard.php?category=" + selectedCategoryId;
   });
 </script>
-</div>
 </body>
 </html>
