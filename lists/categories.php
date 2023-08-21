2c45d5682d3044236e0d780997e1a616d94afa4e
gfaUnDead <contact@gfaundead.stream>
2023-08-21 17:04:02 +1000

    Update categories.php

--- lists/categories.php
+++ lists/categories.php
@@ -61,7 +61,7 @@ if (!$result) {
 <nav class="top-bar stacked-for-medium" id="mobile-menu">
   <div class="top-bar-left">
     <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
-      <li class="menu-text">YourListOnline</li>
+      <li class="menu-text menu-text-black">YourListOnline</li>
       <li><a href="dashboard.php">Dashboard</a></li>
       <li><a href="insert.php">Add</a></li>
       <li><a href="remove.php">Remove</a></li>
@@ -109,7 +109,6 @@ if (!$result) {
 </nav>
 <!-- /Navigation -->
 
-<div class="dark-mode-content">
 <div class="row column">
 <br>
 <h1><?php echo "$greeting, $username!"; ?></h1>
@@ -146,6 +145,5 @@ if (!$result) {
     window.location.href = "categories.php?category=" + selectedCategoryId;
   });
 </script>
-</div>
 </body>
 </html>
