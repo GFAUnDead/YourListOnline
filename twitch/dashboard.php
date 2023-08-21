357eb420e9335fff123a5cd30b1fb159924e814f
gfaUnDead <contact@gfaundead.stream>
2023-08-21 17:00:59 +1000

    Update dashboard.php

--- twitch/dashboard.php
+++ twitch/dashboard.php
@@ -89,7 +89,7 @@ if (!$result) {
 <nav class="top-bar stacked-for-medium" id="mobile-menu">
   <div class="top-bar-left">
     <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
-      <li class="menu-text">YourListOnline</li>
+      <li class="menu-text menu-text-black">YourListOnline</li>
       <li class="is-active"><a href="dashboard.php">Dashboard</a></li>
       <li><a href="insert.php">Add</a></li>
       <li><a href="remove.php">Remove</a></li>
@@ -136,7 +136,6 @@ if (!$result) {
 </nav>
 <!-- /Navigation -->
 
-<div class="dark-mode-content">
 <div class="row column">
 <br>
 <h1><?php echo "$greeting, <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>$twitchDisplayName!"; ?></h1>
@@ -211,6 +210,5 @@ if (!$result) {
     window.location.href = "dashboard.php?category=" + selectedCategoryId;
   });
 </script>
-</div>
 </body>
 </html>
