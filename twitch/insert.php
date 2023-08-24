518d985a7fee9d32f02faee865033fdc1f0de0fa
gfaUnDead <contact@gfaundead.stream>
2023-08-24 20:11:43 +1000

    Update insert.php

--- twitch/insert.php
+++ twitch/insert.php
@@ -91,9 +91,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       <li>
         <a>Profile</a>
         <ul class="vertical menu" data-dropdown-menu>
-					<li><a href="profile.php">View Profile</a></li>
-					<li><a href="update_profile.php">Update Profile</a></li>
+          <li><a href="profile.php">View Profile</a></li>
+          <li><a href="update_profile.php">Update Profile</a></li>
           <li><a href="obs_options.php">OBS Viewing Options</a></li>
+          <li><a href="twitch_mods.php">Twitch Mods</a></li>
           <li><a href="logout.php">Logout</a></li>
         </ul>
       </li>
