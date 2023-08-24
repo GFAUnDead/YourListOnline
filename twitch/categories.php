0944916644171887883f17d5a26dd4c80faa4314
gfaUnDead <contact@gfaundead.stream>
2023-08-24 20:11:40 +1000

    Update categories.php

--- twitch/categories.php
+++ twitch/categories.php
@@ -89,8 +89,8 @@ if (!$result) {
       <li>
         <a>Profile</a>
         <ul class="vertical menu" data-dropdown-menu>
-					<li><a href="profile.php">View Profile</a></li>
-					<li><a href="update_profile.php">Update Profile</a></li>
+          <li><a href="profile.php">View Profile</a></li>
+          <li><a href="update_profile.php">Update Profile</a></li>
           <li><a href="obs_options.php">OBS Viewing Options</a></li>
           <li><a href="twitch_mods.php">Twitch Mods</a></li>
           <li><a href="logout.php">Logout</a></li>
