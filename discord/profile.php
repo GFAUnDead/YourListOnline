dbb121dc69a68a5cd9d32934682677e2dd113f80
GFAUnDead <contact@gfaundead.stream>
2023-10-16 10:10:22 +1100

    Update profile.php

--- discord/profile.php
+++ discord/profile.php
@@ -8,19 +8,12 @@ if (!isset($_SESSION['access_token'])) {
     exit();
 }
 
-// Connect to database
+// Require database connection
 require_once "db_connect.php";
 
-// Get the current hour in 24-hour format (0-23)
-$currentHour = date('G');
-// Initialize the greeting variable
-$greeting = '';
-// Check if it's before 12 PM (noon)
-if ($currentHour < 12) {
-    $greeting = "Good morning";
-} else {
-    $greeting = "Good afternoon";
-}
+// Default Timezone Settings
+$defaultTimeZone = 'Etc/UTC';
+$user_timezone = $defaultTimeZone;
 
 // Fetch the user's data from the database based on the access_token
 $access_token = $_SESSION['access_token'];
@@ -36,6 +29,18 @@ $last_login = $user['last_login'];
 $api_key = $user['api_key'];
 $discord_profile_image_url = $user['profile_image'];
 $is_admin = ($user['is_admin'] == 1);
+$user_timezone = $user['timezone'];
+date_default_timezone_set($user_timezone);
+
+// Determine the greeting based on the user's local time
+$currentHour = date('G');
+$greeting = '';
+
+if ($currentHour < 12) {
+    $greeting = "Good morning";
+} else {
+    $greeting = "Good afternoon";
+}
 
 // Convert the stored date and time to UTC using Sydney time zone (AEST/AEDT)
 date_default_timezone_set('Australia/Sydney');
@@ -75,76 +80,84 @@ if ($alpha_user_flag && $beta_user_flag) {
     <div class="title-bar-title">Menu</div>
 </div>
 <nav class="top-bar stacked-for-medium" id="mobile-menu">
-    <div class="top-bar-left">
-        <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
-            <li class="menu-text">YourListOnline</li>
-            <li><a href="dashboard.php">Dashboard</a></li>
-            <li><a href="insert.php">Add</a></li>
-            <li><a href="remove.php">Remove</a></li>
-            <li>
-                <a>Update</a>
-                <ul class="vertical menu" data-dropdown-menu>
-                    <li><a href="update_objective.php">Update Objective</a></li>
-                    <li><a href="update_category.php">Update Objective Category</a></li>
-                </ul>
-            </li>
-            <li><a href="completed.php">Completed</a></li>
-            <li>
-                <a>Categories</a>
-                <ul class="vertical menu" data-dropdown-menu>
-                    <li><a href="categories.php">View Categories</a></li>
-                    <li><a href="add_category.php">Add Category</a></li>
-                </ul>
-            </li>
-            <li>
-                <a>Profile</a>
-                <ul class="vertical menu" data-dropdown-menu>
-                    <li class="is-active"><a href="profile.php">View Profile</a></li>
-                    <li><a href="update_profile.php">Update Profile</a></li>
-                    <li><a href="obs_options.php">OBS Viewing Options</a></li>
-                    <li><a href="logout.php">Logout</a></li>
-                </ul>
-            </li>
-            <?php if ($is_admin) { ?>
-            <li>
-                <a>Admins</a>
-                <ul class="vertical menu" data-dropdown-menu>
-                    <li><a href="../admins/dashboard.php" target="_self">Admin Dashboard</a></li>
-                </ul>
-            </li>
-            <?php } ?>
+<div class="top-bar-left">
+<ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+    <li class="menu-text menu-text-black">YourListOnline</li>
+    <li><a href="dashboard.php">Dashboard</a></li>
+    <li><a href="insert.php">Add</a></li>
+    <li><a href="remove.php">Remove</a></li>
+    <li>
+        <a>Update</a>
+        <ul class="vertical menu" data-dropdown-menu>
+            <li><a href="update_objective.php">Update Objective</a></li>
+            <li><a href="update_category.php">Update Objective Category</a></li>
         </ul>
+    </li>
+    <li><a href="completed.php">Completed</a></li>
+    <li>
+        <a>Categories</a>
+        <ul class="vertical menu" data-dropdown-menu>
+            <li><a href="categories.php">View Categories</a></li>
+            <li><a href="add_category.php">Add Category</a></li>
+        </ul>
+    </li>
+    <li>
+      <a>Profile</a>
+      <ul class="vertical menu" data-dropdown-menu>
+        <li><a href="profile.php">View Profile</a></li>
+        <li><a href="update_profile.php">Update Profile</a></li>
+        <li><a href="obs_options.php">OBS Viewing Options</a></li>
+        <li><a href="logout.php">Logout</a></li>
+      </ul>
+    </li>
+    <?php if ($is_admin) { ?>
+    <li>
+        <a>Admins</a>
+        <ul class="vertical menu" data-dropdown-menu>
+            <li><a href="../admins/dashboard.php" target="_self">Admin Dashboard</a></li>
+        </ul>
+    </li>
+    <?php } ?>
+  </ul>
     </div>
     <div class="top-bar-right">
-        <ul class="menu">
-            <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
-        </ul>
+      <ul class="menu">
+        <li><button id="dark-mode-toggle"><i class="icon-toggle-dark-mode"></i></button></li>
+        <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourListOnline. All rights reserved.</a></li>
+      </ul>
     </div>
 </nav>
 <!-- /Navigation -->
+
 <div class="row column">
     <br>
     <h1><?php echo "<h1>$greeting, $username!</h1>"; ?></h1>
     <h2>Your Profile</h2>
-    <img src="<?php echo $twitch_profile_image_url; ?>" width="150px" height="150px" alt="Twitch Profile Image for <?php echo $username; ?>">
+    <img src="<?php echo $discord_profile_image_url; ?>" width="150px" height="150px" alt="Twitch Profile Image for <?php echo $username; ?>">
     <br><br>
     <p><strong>Your Username:</strong> <?php echo $username; ?></p>
+    <p><strong>Display Name:</strong> <?php echo $twitchDisplayName; ?></p>
     <p><strong>You Joined:</strong> <span id="localSignupDate"></span></p>
     <p><strong>Your Last Login:</strong> <span id="localLastLogin"></span></p>
+    <p><strong>Your Time Zone:</strong> <?php echo $user_timezone; ?></p>
     <p><strong>Tester Status:</strong> <?php echo $tester_status; ?></p>
-    <p><strong>Your API Key:</strong> <span class="api-key-wrapper" style="display: none;"><?php echo $api_key; ?></span></p>
+    <p><strong>Your API Key:</strong> <span class="api-key-wrapper api-text-black" style="display: none;"><?php echo $api_key; ?></span></p>
     <button type="button" class="defult-button" id="show-api-key">Show API Key</button>
     <button type="button" class="defult-button" id="hide-api-key" style="display:none;">Hide API Key</button>
     <br><br>
-    <button class="defult-button" onclick="showOBSInfo()">HOW TO PUT ON YOUR STREAM</button>
+    <button type="button" class="defult-button" onclick="showOBSInfo()">HOW TO PUT ON YOUR STREAM</button>
     <br><br>
-    <?php if ($is_admin) { ?><a href="change_password.php" class="defult-button">Change Password</a><br><br><?php } ?>
-    <a href="logout.php" class="logout-button">Logout</a>
+    <?php if ($is_admin) { ?><a href="https://payments.yourlist.online" type="button" class="defult-button">Payments</a>
+    <br><br><?php } ?>
+    
+    <a href="logout.php" type="button" class="logout-button">Logout</a>
 </div>
+<!-- Include the JavaScript files -->
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://cdn.yourlist.online/js/profile.js"></script>
 <script src="https://cdn.yourlist.online/js/about.js" defer></script>
 <script src="https://cdn.yourlist.online/js/obsbutton.js" defer></script>
+<script src="https://cdn.yourlist.online/js/darkmode.js"></script>
 <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
 <script>$(document).foundation();</script>
 <script src="https://cdn.yourlist.online/js/timezone.js"></script>
