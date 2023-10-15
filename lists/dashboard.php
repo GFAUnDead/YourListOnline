b87defebe45075f76513e87aa5ccae586972f83f
GFAUnDead <contact@gfaundead.stream>
2023-10-16 09:06:12 +1100

    Update dashboard.php

--- lists/dashboard.php
+++ lists/dashboard.php
@@ -8,19 +8,12 @@ if (!isset($_SESSION['loggedin'])) {
   exit();
 }
 
-// Connect to database
+// Require database connection
 require_once "db_connect.php";
 
-// Get the current hour in 24-hour format (0-23)
-$currentHour = date('G');
-// Initialize the greeting variable
-$greeting = '';
-// Check if it's before 12 PM (noon)
-if ($currentHour < 12) {
-    $greeting = "Good morning";
-} else {
-    $greeting = "Good afternoon";
-}
+// Default Timezone Settings
+$defaultTimeZone = 'Etc/UTC';
+$user_timezone = $defaultTimeZone;
 
 
 // Get user information from the database
@@ -31,6 +24,18 @@ $user_data = mysqli_fetch_assoc($result);
 $is_admin = $user_data['is_admin'];
 $username = $user_data['username'];
 $change_password = $user_data['change_password'];
+$user_timezone = $user['timezone'];
+date_default_timezone_set($user_timezone);
+
+// Determine the greeting based on the user's local time
+$currentHour = date('G');
+$greeting = '';
+
+if ($currentHour < 12) {
+    $greeting = "Good morning";
+} else {
+    $greeting = "Good afternoon";
+}
 
 // Check if the query succeeded
 if (!$result) {
