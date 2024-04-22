cf0e7590f3b753655f0b852f6dca47d695f892fa
gfaUnDead <contact@gfaundead.stream>
2024-04-22 21:25:26 +1000

    Update add_category.php

--- twitch/add_category.php
+++ twitch/add_category.php
@@ -119,10 +119,10 @@ if (empty($category_err)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>YourListOnline - Add Category</title>
     <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
-    <link rel="stylesheet" href="https://cdn.yourlist.online/css/custom.css">
-    <script src="https://cdn.yourlist.online/js/about.js"></script>
-  	<link rel="icon" href="https://cdn.yourlist.online/img/logo.png" type="image/png" />
-  	<link rel="apple-touch-icon" href="https://cdn.yourlist.online/img/logo.png">
+    <link rel="stylesheet" href="https://cdn.yourlistonline.com.au/css/custom.css">
+    <script src="https://cdn.yourlistonline.com.au/js/about.js"></script>
+  	<link rel="icon" href="https://cdn.yourlistonline.com.au/img/logo.png" type="image/png" />
+  	<link rel="apple-touch-icon" href="https://cdn.yourlistonline.com.au/img/logo.png">
   </head>
 <body>
 <!-- Navigation -->
@@ -205,7 +205,7 @@ if (empty($category_err)) {
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
-<script src="https://cdn.yourlist.online/js/darkmode.js"></script>
+<script src="https://cdn.yourlistonline.com.au/js/darkmode.js"></script>
 <script>$(document).foundation();</script>
 <script>
   // JavaScript function to handle the category filter change
